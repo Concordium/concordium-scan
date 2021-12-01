270bb5cb36a4667ad9ecf1dcbc78dab4e3b3cf8f
Nikki Strømsnes <nikkistromsnes@gmail.com>
2021-12-01 10:54:02 +0100

    Upgrade Nuxt and update config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,26 +1,3 @@
-import type { NuxtConfig } from '@nuxt/types'
+import { defineNuxtConfig } from 'nuxt3'
 
-const config: NuxtConfig = {
-  build: {},
-  buildModules: [
-    '@nuxt/typescript-build'
-  ],
-  css: [],
-  env: {},
-  head: {
-    title: 'nuxt-community/typescript-template',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A boilerplate to start a Nuxt+TS project quickly' }
-    ],
-    link: []
-  },
-  loading: { color: '#0c64c1' },
-  modules: [],
-  plugins: [
-    '~/plugins/truncate'
-  ]
-}
-
-export default config
+export default defineNuxtConfig({})
