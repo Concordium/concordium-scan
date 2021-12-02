4321fed7f1fa2bf46086a97253cc686dba9b794e
Martin Gildenpfennig <martin@fintech.builders>
2021-12-02 09:40:45 +0100

    Fixed some errors in creating signer from encrypted wallet

--- backend/ConcordiumSdk/Transactions/Ed25519TransactionSigner.cs
+++ backend/ConcordiumSdk/Transactions/Ed25519TransactionSigner.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text.Json;
-using System.Text.Json.Nodes;
 using ConcordiumSdk.Types;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using NSec.Cryptography;
@@ -21,32 +20,25 @@ public class Ed25519TransactionSigner : ITransactionSigner
     public static Ed25519TransactionSigner CreateFromConcordiumWalletExportFile(
         string exportedWalletFilePath, string password, AccountAddress signerAccount)
     {
-        var encryptedWallet = File.ReadAllText($"./{exportedWalletFilePath}");
-        var root = JsonNode.Parse(encryptedWallet);
-
-        var cipherText = root["cipherText"].GetValue<string>();
-        var metadata = root["metadata"];
-        var initializationVector = metadata["InitializationVector"].GetValue<string>();
-        var iterationCount = metadata["iterations"].GetValue<int>(); 
-        var salt = metadata["salt"].GetValue<string>();
+        var encrypted = GetEncryptedWallet(exportedWalletFilePath);
 
         var key = KeyDerivation.Pbkdf2(
             password: password,
-            salt: Convert.FromBase64String(salt),
+            salt: Convert.FromBase64String(encrypted.Metadata.Salt),
             prf: KeyDerivationPrf.HMACSHA256,
-            iterationCount: iterationCount,
+            iterationCount: encrypted.Metadata.Iterations,
             numBytesRequested: 32);
 
         using var myAes = new AesManaged
         {
             Mode = CipherMode.CBC,
-            IV = Convert.FromBase64String(initializationVector),
+            IV = Convert.FromBase64String(encrypted.Metadata.InitializationVector),
             Key = key,
             Padding = PaddingMode.PKCS7
         };
         using var decryptor = myAes.CreateDecryptor();
 
-        using var ms = new MemoryStream(Convert.FromBase64String(cipherText));
+        using var ms = new MemoryStream(Convert.FromBase64String(encrypted.CipherText));
         using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
         using var jsonDocument = JsonDocument.Parse(cs);
         var result = jsonDocument.RootElement
@@ -66,10 +58,46 @@ public class Ed25519TransactionSigner : ITransactionSigner
         return new Ed25519TransactionSigner(signKeyBytes);
     }
 
+    private static EncryptedWallet GetEncryptedWallet(string exportedWalletFilePath)
+    {
+        var encryptedWallet = File.ReadAllText(exportedWalletFilePath);
+
+        var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true};
+        var result = JsonSerializer.Deserialize<EncryptedWallet>(encryptedWallet, options);
+        if (result == null) throw new InvalidOperationException("Deserialization returned null which is unexpected.");
+        return result;
+    }
+
     public byte[] Sign(byte[] bytes)
     {
         var algorithm = SignatureAlgorithm.Ed25519;
         using var key = Key.Import(algorithm,_privateKey, KeyBlobFormat.RawPrivateKey);
         return algorithm.Sign(key, bytes);
+    }
+
+    private class EncryptedWallet
+    {
+        public EncryptedWallet(string cipherText, EncryptedWalletMetadata metadata)
+        {
+            CipherText = cipherText ?? throw new ArgumentNullException(nameof(cipherText));
+            Metadata = metadata ?? throw new ArgumentNullException(nameof(metadata));
+        }
+
+        public string CipherText { get; private set; }
+        public EncryptedWalletMetadata Metadata { get; private set; }
+    }
+
+    private class EncryptedWalletMetadata
+    {
+        public EncryptedWalletMetadata(string initializationVector, int iterations, string salt)
+        {
+            InitializationVector = initializationVector ?? throw new ArgumentNullException(nameof(initializationVector));
+            Iterations = iterations;
+            Salt = salt ?? throw new ArgumentNullException(nameof(salt));
+        }
+
+        public string InitializationVector { get; }
+        public int Iterations { get; }
+        public string Salt { get; }
     }
 }
