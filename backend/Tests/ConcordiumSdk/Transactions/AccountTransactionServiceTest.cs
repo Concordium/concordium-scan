5e4a138a23da9488167a3bc19ae356c925890adc
Martin Gildenpfennig <martin@fintech.builders>
2021-12-02 10:08:41 +0100

    Updated AccountTransactionServiceTest to be prepare for running both stubbe and non-stubbed

--- backend/Tests/ConcordiumSdk/Transactions/AccountTransactionServiceTest.cs
+++ backend/Tests/ConcordiumSdk/Transactions/AccountTransactionServiceTest.cs
@@ -3,34 +3,72 @@ using ConcordiumSdk.NodeApi;
 using ConcordiumSdk.Transactions;
 using ConcordiumSdk.Types;
 using Tests.TestUtilities.Stubs;
+using Xunit.Abstractions;
 
 namespace Tests.ConcordiumSdk.Transactions;
 
-public class AccountTransactionServiceTest
+/// <summary>
+/// Tests in this suite can be run both with stubs (no transactions will actually happen) and with a real client
+/// and signer (will place actual transactions on a Concordium Node). Default is to run with stubs.
+///
+/// To use real client and signer you need to modify the constructor and enter valid values for both Node-settings
+/// and exported wallet file.
+/// </summary>
+public class AccountTransactionServiceTest : IDisposable
 {
-    private readonly INodeClient _nodeClient;
+    private readonly ITestOutputHelper _output;
     private readonly AccountTransactionService _target;
-    private TransactionSignerStub _signer;
+    private readonly AccountAddress _senderAddress;
+    private readonly INodeClient _nodeClient;
+    private readonly ITransactionSigner _signer;
+    private readonly HttpClient _httpClient;
 
-    public AccountTransactionServiceTest()
+    public AccountTransactionServiceTest(ITestOutputHelper output)
     {
-        _nodeClient = CreateFakeNodeClient();
+        _output = output;
+        _senderAddress = new AccountAddress("31JA2dWnv6xHrdP73kLKvWqr5RMfqoeuJXG2Mep1iyQV9E5aSd");
+
+        if (true)
+        {
+            _nodeClient = new NodeClientStub();
+            _signer = new TransactionSignerStub();
+        }
+        else
+        {
+            var grpcClientSettings = new GrpcNodeClientSettings
+            {
+                Address = "http://<your-node-ip-address>:10000",
+                AuthenticationToken = "your-auth-token"
+            };
+            var exportedWalletFilePath = @"c:\temp\export.concordiumwallet";
+            var exportedWalletPassword = "your-password";
+
+            _httpClient = new HttpClient();
+            _nodeClient = new GrpcNodeClient(grpcClientSettings, _httpClient);
+
+            _signer = Ed25519TransactionSigner.CreateFromConcordiumWalletExportFile(exportedWalletFilePath, exportedWalletPassword, _senderAddress);
+        }
+
         _target = new AccountTransactionService(_nodeClient);
-        _signer = new TransactionSignerStub();
+    }
+
+    public void Dispose()
+    {
+        if (_httpClient != null) _httpClient.Dispose();
     }
 
     [Fact]
     public async Task SendAccountTransactionAsync_SingleTransfer()
     {
-        var sender = new AccountAddress("31JA2dWnv6xHrdP73kLKvWqr5RMfqoeuJXG2Mep1iyQV9E5aSd");
-        
-        var toAddress = new AccountAddress("4hT1SmAHGbRH5m5UocN6xhUv9SXcs7HbNUPHqhS8Zy5jKubU1J");
+        var toAddress = new AccountAddress("3uHj5LudVeJMZ7xAm3E4bbFwN61N4ijb9KtnAuARkhoAMLiNYa");
         var amount = CcdAmount.FromCcd(100);
         var payload = new SimpleTransferPayload(amount, toAddress);
 
-        var txHash = await _target.SendAccountTransactionAsync(sender, payload, _signer);
+        var txHash = await _target.SendAccountTransactionAsync(_senderAddress, payload, _signer);
 
         Assert.Equal(32, txHash.AsBytes.Length);
+
+        _output.WriteLine($"txHash1: {txHash.AsString}");
     }
 
     /// <summary>
@@ -41,43 +79,28 @@ public class AccountTransactionServiceTest
     [Fact]
     public async Task SendAccountTransactionAsync_MultipleTransferFromSameSender()
     {
-        var sender = new AccountAddress("31JA2dWnv6xHrdP73kLKvWqr5RMfqoeuJXG2Mep1iyQV9E5aSd");
-        
         var amount = CcdAmount.FromCcd(100);
-        var toAddress1 = new AccountAddress("4hT1SmAHGbRH5m5UocN6xhUv9SXcs7HbNUPHqhS8Zy5jKubU1J");
+        var toAddress1 = new AccountAddress("3uHj5LudVeJMZ7xAm3E4bbFwN61N4ijb9KtnAuARkhoAMLiNYa");
         var toAddress2 = new AccountAddress("4hT1SmAHGbRH5m5UocN6xhUv9SXcs7HbNUPHqhS8Zy5jKubU1J");
-        var toAddress3 = new AccountAddress("4hT1SmAHGbRH5m5UocN6xhUv9SXcs7HbNUPHqhS8Zy5jKubU1J");
+        var toAddress3 = new AccountAddress("3itG8CR6uRhvdSZtxN1DWvyPdmHsdBpMjic4GFrpqebcWZAKwU");
 
-        var nextAccountNonceResponse = await _nodeClient.GetNextAccountNonceAsync(sender);
+        var nextAccountNonceResponse = await _nodeClient.GetNextAccountNonceAsync(_senderAddress);
         
         var nextAccountNonce = nextAccountNonceResponse.Nonce;
-        var txHash1 = await _target.SendAccountTransactionAsync(sender, nextAccountNonce, new SimpleTransferPayload(amount, toAddress1), _signer);
+        var txHash1 = await _target.SendAccountTransactionAsync(_senderAddress, nextAccountNonce, new SimpleTransferPayload(amount, toAddress1), _signer);
 
         nextAccountNonce = nextAccountNonce.Increment();
-        var txHash2 = await _target.SendAccountTransactionAsync(sender, nextAccountNonce, new SimpleTransferPayload(amount, toAddress2), _signer);
+        var txHash2 = await _target.SendAccountTransactionAsync(_senderAddress, nextAccountNonce, new SimpleTransferPayload(amount, toAddress2), _signer);
         
         nextAccountNonce = nextAccountNonce.Increment();
-        var txHash3 = await _target.SendAccountTransactionAsync(sender, nextAccountNonce, new SimpleTransferPayload(amount, toAddress3), _signer);
+        var txHash3 = await _target.SendAccountTransactionAsync(_senderAddress, nextAccountNonce, new SimpleTransferPayload(amount, toAddress3), _signer);
 
+        _output.WriteLine($"txHash1: {txHash1.AsString}");
+        _output.WriteLine($"txHash2: {txHash2.AsString}");
+        _output.WriteLine($"txHash3: {txHash3.AsString}");
+        
         Assert.Equal(32, txHash1.AsBytes.Length);
         Assert.Equal(32, txHash2.AsBytes.Length);
         Assert.Equal(32, txHash3.AsBytes.Length);
-    }
-
-    private INodeClient CreateFakeNodeClient()
-    {
-        return new NodeClientStub();
-    }
-
-    private static INodeClient CreateRealNodeClient()
-    {
-        using var httpClient = new HttpClient();
-        var grpcClientSettings = new GrpcNodeClientSettings
-        {
-            Address = "http://40.127.163.29:10000",
-            AuthenticationToken = "FTBgrpc2021"
-        };
-        var grpcClient = new GrpcNodeClient(grpcClientSettings, httpClient);
-        return grpcClient;
     }
 }
