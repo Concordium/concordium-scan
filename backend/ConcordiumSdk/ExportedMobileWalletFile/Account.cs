b4c1b6cbc392d4b68d240f8a24a8dd8e44593f97
Martin Gildenpfennig <martin@fintech.builders>
2021-12-03 10:18:59 +0100

    Added name to Account when deserializing mobile wallet file

--- backend/ConcordiumSdk/ExportedMobileWalletFile/Account.cs
+++ backend/ConcordiumSdk/ExportedMobileWalletFile/Account.cs
@@ -4,12 +4,14 @@ namespace ConcordiumSdk.ExportedMobileWalletFile;
 
 public class Account
 {
-    public Account(AccountAddress address, AccountKeysRootContainer accountKeys)
+    public Account(AccountAddress address, AccountKeysRootContainer accountKeys, string name)
     {
         Address = address;
         AccountKeys = accountKeys;
+        Name = name;
     }
 
     public AccountAddress Address { get; }
     public AccountKeysRootContainer AccountKeys { get; }
+    public string Name { get; }
 }
