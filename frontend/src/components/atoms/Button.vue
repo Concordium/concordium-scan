b2ceb45ace71c943b8c0ee4c560b13bac1e61526
Nikki Strømsnes <nikki@fintech.builders>
2021-12-22 10:20:47 +0100

    fix: use correct PropType import

--- frontend/src/components/atoms/Button.vue
+++ frontend/src/components/atoms/Button.vue
@@ -8,7 +8,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent } from 'vue'
+import type { PropType } from 'vue'
 
 export default defineComponent({
 	name: 'Button',
