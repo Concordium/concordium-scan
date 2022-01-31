54554727fe3b947644b4da1bd8f131e062a5e6db
Martin Gildenpfennig <martin@fintech.builders>
2022-01-31 19:51:27 +0100

    Terraform: Added application gateway pure HTTP

--- Terraform/Backends/locals.tf
+++ Terraform/Backends/locals.tf
@@ -5,11 +5,13 @@ locals {
           resource_group_name = "ccscan-test"
           azure_vm_size = "Standard_B4ms"
           container_repository_backend = "backend.test"
+          public_host_name_mainnet_backend = "test.api-mainnet.ccdscan.io"
+          public_host_name_testnet_backend = "test.api-testnet.ccdscan.io"
       }
   }
 
   vm_admin_user = "concNodeVMuser"
-  
+
   # TODO: Secrets should probably go to the key vault instead
   vm_entrypoint_script = templatefile("${path.cwd}/entrypoint.tpl", {
     vm_user = local.vm_admin_user
--- Terraform/Backends/main.tf
+++ Terraform/Backends/main.tf
@@ -18,8 +18,16 @@ resource "azurerm_virtual_network" "this" {
   }
 }
 
-resource "azurerm_subnet" "this" {
-  name                 = "snet"
+resource "azurerm_subnet" "frontend" {
+  name                 = "snet-frontend"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+
+resource "azurerm_subnet" "backend" {
+  name                 = "snet-backend"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -131,12 +139,24 @@ resource "azurerm_network_security_group" "this" {
   }
 }
 
+resource "azurerm_public_ip" "app-gw" {
+  name                = "pip-app-gw"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  allocation_method   = "Dynamic"
+  domain_name_label   = "${local.environment}-ccdscan-api"
+
+  tags = {
+    environment = local.environment
+  }
+}
+
 resource "azurerm_public_ip" "vm" {
   name                = "pip-vm"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   allocation_method   = "Static"
-  domain_name_label   = "ftbccscantestnode"
+  domain_name_label   = "${local.environment}-ccdscan-vm"
 
   tags = {
     environment = local.environment
@@ -149,8 +169,8 @@ resource "azurerm_network_interface" "vm" {
   resource_group_name = azurerm_resource_group.this.name
 
   ip_configuration {
-    name                          = "ipc-concNodeVM"
-    subnet_id                     = azurerm_subnet.this.id
+    name                          = "ipc-vm"
+    subnet_id                     = azurerm_subnet.backend.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.vm.id
   }
@@ -226,4 +246,105 @@ resource "azurerm_virtual_machine_data_disk_attachment" "vm_data" {
   virtual_machine_id = azurerm_linux_virtual_machine.vm.id
   lun                = "1"
   caching            = "ReadWrite"
+}
+
+resource "azurerm_application_gateway" "this" {
+  name                = "app-gateway"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  sku {
+    name     = "Standard_Small"
+    tier     = "Standard"
+    capacity = 1
+  }
+
+  gateway_ip_configuration {
+    name      = "gateway-ip-configuration"
+    subnet_id = azurerm_subnet.frontend.id
+  }
+
+  frontend_port {
+    name = "app-gateway-feport"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "app-gateway-feip"
+    public_ip_address_id = azurerm_public_ip.app-gw.id
+  }
+
+  backend_address_pool {
+    name = "app-gateway-backends"
+  }
+
+  backend_http_settings {
+    name                  = "mainnet-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 5000
+    protocol              = "Http"
+    request_timeout       = 20
+    probe_name            = "health-probe"
+    pick_host_name_from_backend_address = true
+  }
+
+  backend_http_settings {
+    name                  = "testnet-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 5001
+    protocol              = "Http"
+    request_timeout       = 20
+    probe_name            = "health-probe"
+    pick_host_name_from_backend_address = true
+  }
+
+  http_listener {
+    name                           = "mainnet-listener"
+    frontend_ip_configuration_name = "app-gateway-feip"
+    frontend_port_name             = "app-gateway-feport"
+    # TODO: change to https and add ssl
+    protocol                       = "Http"
+    host_name                      = local.envs[local.environment].public_host_name_mainnet_backend
+  }
+
+  http_listener {
+    name                           = "testnet-listener"
+    frontend_ip_configuration_name = "app-gateway-feip"
+    frontend_port_name             = "app-gateway-feport"
+    # TODO: change to https and add ssl
+    protocol                       = "Http"
+    host_name                      = local.envs[local.environment].public_host_name_testnet_backend
+  }
+
+  request_routing_rule {
+    name                       = "mainnet-redirect"
+    rule_type                  = "Basic"
+    http_listener_name         = "mainnet-listener"
+    backend_address_pool_name  = "app-gateway-backends"
+    backend_http_settings_name = "mainnet-http-settings"
+  }
+
+  request_routing_rule {
+    name                       = "testnet-redirect"
+    rule_type                  = "Basic"
+    http_listener_name         = "testnet-listener"
+    backend_address_pool_name  = "app-gateway-backends"
+    backend_http_settings_name = "testnet-http-settings"
+  }
+
+  probe {
+    name = "health-probe"
+    protocol = "Http"
+    path = "/api/health"
+    interval = 30
+    timeout = 30
+    unhealthy_threshold = 3
+    pick_host_name_from_backend_http_settings = true
+  }
+}
+
+resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "vm" {
+  network_interface_id    = azurerm_network_interface.vm.id
+  ip_configuration_name   = "ipc-vm"
+  backend_address_pool_id = azurerm_application_gateway.this.backend_address_pool[0].id
 }
