28692f00cde02dab26d64ab9b569f0e2a4dccc7b
Martin Gildenpfennig <martin@fintech.builders>
2022-02-02 13:46:55 +0100

    fix: fixed an error in bootstrap ssl script that caused it to export the wrong pfx

--- Terraform/Scripts/GenerateBootstrapSslCertAndPushToKeyVault.ps1
+++ Terraform/Scripts/GenerateBootstrapSslCertAndPushToKeyVault.ps1
@@ -17,8 +17,8 @@ $pArgs = @{
 New-PACertificate "*.${domain}", "${domain}" -AcceptTOS -Contact $contactEmail -Plugin GoDaddy -PluginArgs $pArgs -PfxPass $pfxPassword -Verbose -Force
 
 # Push result to Azure Key Vault:
-$result = Get-PACertificate *.dev-api.ccdscan.io | 
+$result = Get-PACertificate "*.${domain}" | 
     Select-Object -Property PfxFile, @{name='Expires'; expression={$_.NotAfter.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssK")}}, @{name='NotBefore'; expression={$_.NotBefore.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssK")}}
 
-az keyvault secret set --vault-name ccscan --name "${environmentName}-sslcert-pfx" --file $result.PfxFile --encoding base64 --not-before $result.NotBefore --expires $result.Expires
+az keyvault secret set --vault-name ccscan --name "${environmentName}-sslcert-pfx" --file $result.PfxFile --encoding base64 --not-before $result.NotBefore --expires $result.Expires --description "Base64 encoded PFX"
 az keyvault secret set --vault-name ccscan --name "${environmentName}-sslcert-pfx-password" --value $pfxPassword 
