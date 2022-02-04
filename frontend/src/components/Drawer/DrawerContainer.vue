b87a2271f8449835858a4c8e3d975ca7397b9a49
Magnus <FalkenFlyver@users.noreply.github.com>
2022-02-04 11:33:44 +0100

    Redo the drawer to use teh same transition code as the old drawer.

--- frontend/src/components/Drawer/DrawerContainer.vue
+++ frontend/src/components/Drawer/DrawerContainer.vue
@@ -1,18 +1,27 @@
 ﻿<template>
-	<div v-if="currentTopItem">
-		<BlockDetailsDrawer
-			v-if="currentTopItem.entityTypeName == 'block'"
-			:id="currentTopItem.id"
-		/>
-		<TransactionDetailsDrawer
-			v-if="currentTopItem.entityTypeName == 'transaction'"
-			:id="currentTopItem.id"
-		/>
-	</div>
+	<Drawer :is-open="!!currentTopItem?.id" :on-close="() => softReset()">
+		<template #content>
+			<BlockDetailsContainer
+				v-if="currentTopItem && currentTopItem.entityTypeName == 'block'"
+				:id="currentTopItem?.id"
+			/>
+			<TransactionDetailsContainer
+				v-if="currentTopItem && currentTopItem.entityTypeName == 'transaction'"
+				:id="currentTopItem?.id"
+			/>
+		</template>
+
+		<template #actions>
+			<DrawerActions>
+				<Button class="self-end" :on-click="() => softReset()"> Close </Button>
+			</DrawerActions>
+		</template>
+	</Drawer>
 </template>
 
 <script lang="ts" setup>
 import { useDrawer } from '~/composables/useDrawer'
-
+import Drawer from '~/components/Drawer/Drawer.vue'
+const { softReset } = useDrawer()
 const { currentTopItem } = useDrawer()
 </script>
