cc1bc97d29cd9a9febd19aceb249797b43a70670
Martin Gildenpfennig <martin@fintech.builders>
2022-02-09 11:50:27 +0100

    fix: account metrics graphql query fields renamed and comments added to graphql schema

--- backend/Application/Api/GraphQL/Metrics/AccountsMetrics.cs
+++ backend/Application/Api/GraphQL/Metrics/AccountsMetrics.cs
@@ -1,12 +1,20 @@
-﻿namespace Application.Api.GraphQL.Metrics;
+﻿using HotChocolate;
+
+namespace Application.Api.GraphQL.Metrics;
 
 public record AccountsMetrics(
+    [property:GraphQLDescription("Total number of accounts created (all time)")]
     long LastCumulativeAccountsCreated, 
-    int SumAccountsCreated, 
+    [property:GraphQLDescription("Total number of accounts created in requested period.")]
+    int AccountsCreated, 
     AccountsMetricsBuckets Buckets);
     
 public record AccountsMetricsBuckets(
+    [property:GraphQLDescription("The width (time interval) of each bucket.")]
     TimeSpan BucketWidth,
+    [property:GraphQLDescription("Start of the bucket time period. Intended x-axis value.")]
     DateTimeOffset[] X_Time,
+    [property:GraphQLDescription("Total number of accounts created (all time) at the end of the bucket period. Intended y-axis value.")]
     long[] Y_LastCumulativeAccountsCreated,
-    int[] Y_SumAccountsCreated);
+    [property:GraphQLDescription("Number of accounts created within bucket time period. Intended y-axis value.")]
+    int[] Y_AccountsCreated);
