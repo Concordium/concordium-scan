90b9186a99aeb9a6420c0b484c2b9a1d9b243258
Magnus <FalkenFlyver@users.noreply.github.com>
2022-02-28 10:55:14 +0100

    Wrapped KeyValueChartCard in a ClientOnly to remove the hydration warning

--- frontend/src/components/molecules/KeyValueChartCard.vue
+++ frontend/src/components/molecules/KeyValueChartCard.vue
@@ -1,46 +1,48 @@
 ﻿<template>
-	<div
-		class="flex flex-row cardShadow rounded-2xl shadow-2xl m-4 relative overflow-hidden bg-theme-background-primary-elevated"
-	>
-		<div class="flex flex-col items-center w-full">
-			<div class="absolute top-4 right-4 text-xs">
-				<slot name="topRight"></slot>
-			</div>
-
-			<div class="text-sm text-theme-faded pt-4 w-72 text-center">
-				<slot name="title"></slot>
-			</div>
-
-			<div class="text-xl text-theme-interactive flex flex-row gap-2">
-				<div class="w-6 h-6 mr-2 text-theme-interactive">
-					<slot name="icon"></slot>
+	<ClientOnly>
+		<div
+			class="flex flex-row cardShadow rounded-2xl shadow-2xl m-4 relative overflow-hidden bg-theme-background-primary-elevated"
+		>
+			<div class="flex flex-col items-center w-full">
+				<div class="absolute top-4 right-4 text-xs">
+					<slot name="topRight"></slot>
 				</div>
-				<div><slot name="value"></slot></div>
-				<div><slot name="unit"></slot></div>
-				<Chip class="self-center"><slot name="chip"></slot></Chip>
-			</div>
-			<div v-if="props.chartType == 'area'" class="h-full w-full">
-				<ChartLineArea
-					v-if="props.xValues && props.yValues"
-					class="h-28"
-					:x-values="props.xValues"
-					:y-values-high="props.yValues[0]"
-					:y-values-mid="props.yValues[1]"
-					:y-values-low="props.yValues[2]"
-					:bucket-width="props.bucketWidth"
-				></ChartLineArea>
-			</div>
-			<div v-else class="h-full w-full">
-				<ChartLine
-					v-if="props.xValues && props.yValues"
-					class="h-28"
-					:x-values="props.xValues"
-					:y-values="props.yValues"
-					:bucket-width="props.bucketWidth"
-				></ChartLine>
+
+				<div class="text-sm text-theme-faded pt-4 w-72 text-center">
+					<slot name="title"></slot>
+				</div>
+
+				<div class="text-xl text-theme-interactive flex flex-row gap-2">
+					<div class="w-6 h-6 mr-2 text-theme-interactive">
+						<slot name="icon"></slot>
+					</div>
+					<div><slot name="value"></slot></div>
+					<div><slot name="unit"></slot></div>
+					<Chip class="self-center"><slot name="chip"></slot></Chip>
+				</div>
+				<div v-if="props.chartType == 'area'" class="h-full w-full">
+					<ChartLineArea
+						v-if="props.xValues && props.yValues"
+						class="h-28"
+						:x-values="props.xValues"
+						:y-values-high="props.yValues[0]"
+						:y-values-mid="props.yValues[1]"
+						:y-values-low="props.yValues[2]"
+						:bucket-width="props.bucketWidth"
+					></ChartLineArea>
+				</div>
+				<div v-else class="h-full w-full">
+					<ChartLine
+						v-if="props.xValues && props.yValues"
+						class="h-28"
+						:x-values="props.xValues"
+						:y-values="props.yValues"
+						:bucket-width="props.bucketWidth"
+					></ChartLine>
+				</div>
 			</div>
 		</div>
-	</div>
+	</ClientOnly>
 </template>
 <script lang="ts" setup>
 import ChartLine from '~/components/Charts/ChartLine.vue'
