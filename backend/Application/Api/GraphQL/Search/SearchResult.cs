76ce6958961fd117d567a572b6a2dfd5648864a2
Martin Gildenpfennig <martin@fintech.builders>
2022-03-01 09:14:12 +0100

    feat: basic paging on search results

--- backend/Application/Api/GraphQL/Search/SearchResult.cs
+++ backend/Application/Api/GraphQL/Search/SearchResult.cs
@@ -1,8 +1,13 @@
-﻿namespace Application.Api.GraphQL.Search;
+﻿using HotChocolate.Types;
+
+namespace Application.Api.GraphQL.Search;
 
 public class SearchResult
 {
+    [UsePaging]
     public Block[] Blocks { get; set; } = Array.Empty<Block>();
+    [UsePaging]
     public Transaction[] Transactions { get; set; } = Array.Empty<Transaction>();
+    [UsePaging]
     public Account[] Accounts { get; set; } = Array.Empty<Account>();
 }
