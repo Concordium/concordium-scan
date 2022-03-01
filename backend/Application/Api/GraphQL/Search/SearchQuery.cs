30f984737adaec9421c4c6c872e07c705de959e4
Martin Gildenpfennig <martin@fintech.builders>
2022-03-01 10:26:56 +0100

    Structured graphql search query internally for better performance by allowing paging to be done directly in database. New structure also enables that only the fields selected by client (blocks, transaction, accounts) are queried from database (no over-fetching of entity types from db)

--- backend/Application/Api/GraphQL/Search/SearchQuery.cs
+++ backend/Application/Api/GraphQL/Search/SearchQuery.cs
@@ -1,43 +1,12 @@
-﻿using System.Threading.Tasks;
-using Application.Api.GraphQL.EfCore;
-using HotChocolate;
-using HotChocolate.Types;
-using Microsoft.EntityFrameworkCore;
+﻿using HotChocolate.Types;
 
 namespace Application.Api.GraphQL.Search;
 
 [ExtendObjectType(typeof(Query))]
 public class SearchQuery
 {
-    public async Task<SearchResult> Search([Service] IDbContextFactory<GraphQlDbContext> dbContextFactory, string query)
+    public SearchResult Search(string query)
     {
-        var isQueryNumeric = long.TryParse(query, out var queryNumeric);
-            
-        await using var blocksDbContext = await dbContextFactory.CreateDbContextAsync();
-        var blocksTask = blocksDbContext.Blocks.AsNoTracking()
-            .Where(block => block.BlockHash.StartsWith(query) || isQueryNumeric && block.BlockHeight == queryNumeric)
-            .OrderByDescending(block => block.Id)
-            .ToArrayAsync();
-
-        await using var transactionsDbContext = await dbContextFactory.CreateDbContextAsync();
-        var transactionsTask = transactionsDbContext.Transactions.AsNoTracking()
-            .Where(transaction => transaction.TransactionHash.StartsWith(query))
-            .OrderByDescending(transaction => transaction.Id)
-            .ToArrayAsync();
-
-        await using var accountsDbContext = await dbContextFactory.CreateDbContextAsync();
-        var accountsTask = accountsDbContext.Accounts.AsNoTracking()
-            .Where(account => account.Address.StartsWith(query))
-            .OrderByDescending(account => account.Id)
-            .ToArrayAsync();
-
-        await Task.WhenAll(blocksTask, transactionsTask, accountsTask);
-        
-        return new SearchResult
-        {
-            Blocks = blocksTask.Result,
-            Transactions = transactionsTask.Result,
-            Accounts = accountsTask.Result
-        };
+        return new SearchResult(query);
     }
 }
--- backend/Application/Api/GraphQL/Search/SearchResult.cs
+++ backend/Application/Api/GraphQL/Search/SearchResult.cs
@@ -1,13 +1,48 @@
-﻿using HotChocolate.Types;
+﻿using Application.Api.GraphQL.EfCore;
+using HotChocolate;
+using HotChocolate.Data;
+using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Api.GraphQL.Search;
 
 public class SearchResult
 {
+    private readonly string _queryString;
+    private readonly long? _queryNumeric;
+
+    public SearchResult(string query)
+    {
+        _queryString = query;
+        var isQueryNumeric = long.TryParse(query, out var queryNumeric);
+        _queryNumeric = isQueryNumeric ? queryNumeric : null;
+    }
+
+    [UseDbContext(typeof(GraphQlDbContext))]
     [UsePaging]
-    public Block[] Blocks { get; set; } = Array.Empty<Block>();
+    public IQueryable<Block> GetBlocks([ScopedService] GraphQlDbContext dbContext)
+    {
+        return dbContext.Blocks.AsNoTracking()
+            .Where(block => block.BlockHash.StartsWith(_queryString) ||
+                            _queryNumeric.HasValue && block.BlockHeight == _queryNumeric.Value)
+            .OrderByDescending(block => block.Id);
+    }
+
+    [UseDbContext(typeof(GraphQlDbContext))]
     [UsePaging]
-    public Transaction[] Transactions { get; set; } = Array.Empty<Transaction>();
+    public IQueryable<Transaction> GetTransactions([ScopedService] GraphQlDbContext dbContext)
+    {
+        return dbContext.Transactions.AsNoTracking()
+            .Where(transaction => transaction.TransactionHash.StartsWith(_queryString))
+            .OrderByDescending(transaction => transaction.Id);
+    }
+
+    [UseDbContext(typeof(GraphQlDbContext))]
     [UsePaging]
-    public Account[] Accounts { get; set; } = Array.Empty<Account>();
+    public IQueryable<Account> GetAccounts([ScopedService] GraphQlDbContext dbContext)
+    {
+        return dbContext.Accounts.AsNoTracking()
+            .Where(account => account.Address.StartsWith(_queryString))
+            .OrderByDescending(account => account.Id);
+    }
 }
