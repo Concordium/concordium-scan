a4ee1aa8cb387d03f1c314738cfa726b1c93824e
Martin Gildenpfennig <martin@fintech.builders>
2022-03-16 17:43:10 +0100

    recreate some indexes to support like operator for searches

---
+++ backend/DatabaseScripts/SqlScripts/0018_Recreate_indexes_to_support_like_operator.sql
@@ -0,0 +1,13 @@
+﻿
+drop index graphql_blocks_block_hash_index;
+create index graphql_blocks_block_hash_index
+    on graphql_blocks (block_hash text_pattern_ops);
+
+drop index graphql_transactions_transaction_hash_index;
+create index graphql_transactions_transaction_hash_index
+    on graphql_transactions (transaction_hash text_pattern_ops);
+
+drop index graphql_accounts_canonical_address_uindex;
+create unique index graphql_accounts_canonical_address_uindex
+    on graphql_accounts (canonical_address text_pattern_ops);
+
