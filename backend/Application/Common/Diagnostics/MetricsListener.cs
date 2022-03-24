a4769d3d9b297705d75a235fa3239d6237d41eb2
Martin Gildenpfennig <martin@fintech.builders>
2022-03-24 12:16:52 +0100

    Format of written diagnostics changed

--- backend/Application/Common/Diagnostics/MetricsListener.cs
+++ backend/Application/Common/Diagnostics/MetricsListener.cs
@@ -37,9 +37,37 @@ public class MetricsListener : IDisposable
 
     public void DumpCapturedMetrics()
     {
+        var rows = _measurements
+            .OrderBy(x => x.Key)
+            .Select(x => new
+            {
+                Name = x.Key,
+                Count = $"{x.Value.Count}",
+                Average = $"{x.Value.Average():F1}ms",
+                Total = $"{x.Value.Sum()}ms",
+                Max = $"{x.Value.Max()}ms",
+            })
+            .ToList();
+        
+        rows.Insert(0, new
+        {
+            Name = "Name",
+            Count = "Count",
+            Average = "Avg",
+            Total = "Total",
+            Max = "Max"
+        });
+        
+        var col0Format = $"{{0, -{rows.Max(x => x.Name.Length)}}}";
+        var col1Format = $"{{1, {rows.Max(x => x.Count.Length)}}}";
+        var col2Format = $"{{2, {rows.Max(x => x.Average.Length)}}}";
+        var col3Format = $"{{3, {rows.Max(x => x.Total.Length)}}}";
+        var col4Format = $"{{4, {rows.Max(x => x.Max.Length)}}}";
+        var rowFormat = $"{Environment.NewLine}   | {col0Format} | {col1Format} | {col2Format} | {col3Format} | {col4Format} |";
+        
         var result = new StringBuilder();
-        foreach (var measurement in _measurements.OrderBy(x => x.Key))
-            result.Append($"{Environment.NewLine}   {measurement.Key} [count:{measurement.Value.Count}] [average:{measurement.Value.Average():F0}ms] [max:{measurement.Value.Max()}ms]");
+        foreach (var row in rows)
+            result.AppendFormat(rowFormat, row.Name, row.Count, row.Average, row.Total, row.Max);
         
         _logger.Information($"Captured metrics: {result}");
         _measurements.Clear();
