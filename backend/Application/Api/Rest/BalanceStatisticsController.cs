ba7d845e68941aae71b4121736ff8bb3135dede3
Martin Gildenpfennig <martin@fintech.builders>
2022-04-19 11:51:57 +0200

    feat: added rest-controller for getting balance statistics for latest import block

---
+++ backend/Application/Api/Rest/BalanceStatisticsController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Application.Api.GraphQL.EfCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Api.Rest;
+
+[ApiController]
+public class BalanceStatisticsController : ControllerBase
+{
+    private readonly IDbContextFactory<GraphQlDbContext> _dbContextFactory;
+
+    public BalanceStatisticsController(IDbContextFactory<GraphQlDbContext> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+    }
+
+    [HttpGet]
+    [Route("rest/balance-statistics/latest")]
+    public async Task<dynamic> GetLatest()
+    {
+        await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
+        
+        var block = await dbContext.Blocks
+            .AsNoTracking()
+            .OrderByDescending(x => x.Id)
+            .FirstAsync();
+
+        return new BalanceStatisticsResponse
+        (
+            block.BlockHeight,
+            block.BlockHash,
+            block.BlockSlotTime,
+            block.BalanceStatistics.TotalAmount,
+            block.BalanceStatistics.TotalAmountReleased,
+            block.BalanceStatistics.TotalAmountStaked,
+            block.BalanceStatistics.TotalAmountEncrypted,
+            block.BalanceStatistics.TotalAmountLockedInReleaseSchedules
+        );
+    }
+
+    private record BalanceStatisticsResponse(
+            long BlockHeight, 
+            string BlockHash, 
+            DateTimeOffset BlockSlotTime,
+            ulong TotalAmount, 
+            ulong? TotalAmountReleased,
+            ulong TotalAmountStaked, 
+            ulong TotalAmountEncrypted,
+            ulong TotalAmountLockedInReleaseSchedules);
+}
--- backend/Application/Api/Rest/HealthController.cs
+++ backend/Application/Api/Rest/HealthController.cs
@@ -7,6 +7,7 @@ namespace Application.Api.Rest;
 /// </summary>
 [ApiController]
 [Route("api/[controller]")]
+[Route("rest/[controller]")]
 public class HealthController : ControllerBase
 {
     [HttpGet]
