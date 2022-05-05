bd5e9d74af986bc1dc58a4d908f7b4a22eb2d9f5
Martin Gildenpfennig <martin@fintech.builders>
2022-05-05 14:08:28 +0200

    fix: baker statistics view now only assigns ranks to active bakers

---
+++ backend/DatabaseScripts/SqlScripts/0029_RecreateMaterializedView_graphql_baker_statistics.sql
@@ -0,0 +1,20 @@
+﻿drop materialized view graphql_baker_statistics;
+
+create materialized view graphql_baker_statistics as
+select
+    id as baker_id,
+    (
+        case active_staked_amount is not null
+            when true then row_number() over (order by active_staked_amount desc nulls last)::int
+            end
+        ) as active_baker_rank_by_stake,
+    (
+        case active_staked_amount is not null
+            when true then (select count(active_staked_amount) from graphql_bakers)::int
+            end
+        ) as active_baker_count,
+    round(active_staked_amount::decimal / (select bal_stats_total_amount from graphql_blocks order by id desc limit 1)::decimal, 10) as active_stake_percentage
+from graphql_bakers;
+
+create unique index graphql_baker_statistics_baker_id_index
+    on graphql_baker_statistics (baker_id);
