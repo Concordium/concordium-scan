7a2408f38efaa4f91049e286913be81e9a48b673
Magnus <FalkenFlyver@users.noreply.github.com>
2022-05-20 10:17:17 +0200

    fix: added external link component to metadata

--- frontend/src/components/TransactionEventList/Events/BakerSetMetadataURL.vue
+++ frontend/src/components/TransactionEventList/Events/BakerSetMetadataURL.vue
@@ -1,13 +1,16 @@
 ﻿<template>
 	<span>
 		Baker <BakerLink :id="event.bakerId" /> set metadata url to
-		{{ event.metadataUrl }}</span
+		<ExternalLink :href="event.metadataUrl">{{
+			event.metadataUrl
+		}}</ExternalLink></span
 	>
 </template>
 
 <script setup lang="ts">
 import BakerLink from '~/components/molecules/BakerLink.vue'
 import type { BakerSetMetadataUrl } from '~/types/generated'
+import ExternalLink from '~/components/molecules/ExternalLink.vue'
 
 type Props = {
 	event: BakerSetMetadataUrl
