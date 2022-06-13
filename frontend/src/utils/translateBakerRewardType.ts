b3c9d55ff5bd00e740caf7178910f5590bfdb0d3
Nikki Strømsnes <nikki@fintech.builders>
2022-06-13 16:34:59 +0200

    refactor: remove deprecated baker rewards types

--- frontend/src/utils/translateBakerRewardType.ts
+++ frontend/src/utils/translateBakerRewardType.ts
@@ -1,5 +1,4 @@
-﻿import type { BakerRewardType } from '~/types/generated'
-import { AccountStatementEntryType, RewardType } from '~/types/generated'
+﻿import type { AccountStatementEntryType, RewardType } from '~/types/generated'
 
 const translations = {
 	entryTypes: {
@@ -8,16 +7,11 @@ const translations = {
 		FINALIZATION_REWARD: 'Finalization reward',
 		TRANSACTION_FEE_REWARD: 'Transaction fee reward',
 		UNKNOWN: 'Unknown',
-	} as Record<
-		BakerRewardType | AccountStatementEntryType | RewardType | 'UNKNOWN',
-		string
-	>,
+	} as Record<AccountStatementEntryType | RewardType | 'UNKNOWN', string>,
 }
+
 export const translateBakerRewardType = (
-	accountStatementEntry:
-		| BakerRewardType
-		| AccountStatementEntryType
-		| RewardType
+	accountStatementEntry: AccountStatementEntryType | RewardType
 ) => {
 	const translationKey = accountStatementEntry || 'UNKNOWN'
 	return (
