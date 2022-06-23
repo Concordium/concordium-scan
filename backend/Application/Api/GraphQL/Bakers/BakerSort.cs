2ae4e0438f322df9949175b27b0d6b5078df9096
Martin Gildenpfennig <martin@fintech.builders>
2022-06-23 14:43:21 +0200

    feat: added option to sort baker list by baker- or delegator apy 30 days

--- backend/Application/Api/GraphQL/Bakers/BakerSort.cs
+++ backend/Application/Api/GraphQL/Bakers/BakerSort.cs
@@ -9,5 +9,7 @@ public enum BakerSort
     TotalStakedAmountAsc,
     TotalStakedAmountDesc,
     DelegatorCountAsc,
-    DelegatorCountDesc
+    DelegatorCountDesc,
+    BakerApy30DaysDesc,
+    DelegatorApy30DaysDesc
 }
