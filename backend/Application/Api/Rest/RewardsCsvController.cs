df207d4b14c2a9c5063de6b4d3755deffdcff12d
Michael Bisgaard Olesen <mo@concordium.com>
2022-09-26 20:55:13 +0200

    Export statement instead of just rewards

--- backend/Application/Api/Rest/RewardsCsvController.cs
+++ backend/Application/Api/Rest/RewardsCsvController.cs
@@ -8,37 +8,37 @@ using Microsoft.EntityFrameworkCore;
 namespace Application.Api.Rest;
 
 [ApiController]
-public class RewardsCsvController : ControllerBase
+public class CsvExportController : ControllerBase
 {
     private readonly IDbContextFactory<GraphQlDbContext> _dbContextFactory;
 
-    public RewardsCsvController(IDbContextFactory<GraphQlDbContext> dbContextFactory)
+    public CsvExportController(IDbContextFactory<GraphQlDbContext> dbContextFactory)
     {
         _dbContextFactory = dbContextFactory;
     }
 
     [HttpGet]
-    [Route("rest/rewards")]
-    public async Task<ActionResult> GetRewards(long accountId)
+    [Route("rest/export/statement")]
+    public async Task<ActionResult> GetStatementExport(long accountId)
     {
         await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
 
-        var query = dbContext.AccountRewards
+        var query = dbContext.AccountStatementEntries
             .AsNoTracking()
             .Where(x => x.AccountId == accountId);
 
         var scalarQuery = query.Select(x => new {
             Timestamp = x.Timestamp,
-            RewardType = x.RewardType,
+            EntryType = x.EntryType,
             Amount = x.Amount,
         });
         var values = await scalarQuery.ToListAsync();
-        // TODO Better to use something like 'CsvHelper' (see 'https://joshclose.github.io/CsvHelper/examples/writing/write-anonymous-type-objects/')?
+        // TODO Use something like 'CsvHelper' (see 'https://joshclose.github.io/CsvHelper/examples/writing/write-anonymous-type-objects/')?
         var sb = new StringBuilder("timestamp,type,amount_uccd\n");
-        var csv = values.Aggregate(sb, (acc, v) => acc.Append($"{v.Timestamp},{v.RewardType},{v.Amount}\n"));
+        var csv = values.Aggregate(sb, (acc, v) => acc.Append($"{v.Timestamp.ToString("u")},{v.EntryType},{v.Amount}\n"));
         var result = new FileContentResult(Encoding.ASCII.GetBytes(csv.ToString()), "text/csv")
         {
-            FileDownloadName = $"rewards-{accountId}.csv",
+            FileDownloadName = $"statement-{accountId}.csv",
         };
         return result;
     }
