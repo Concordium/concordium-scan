76f5cde0d614062c55cb9b6a4c7ff92293e9a3e8
Michael Bisgaard Olesen <mo@concordium.com>
2022-10-26 15:26:42 +0200

    Rename controller class and lookup account by address

--- backend/Application/Api/Rest/ExportController.cs
+++ backend/Application/Api/Rest/ExportController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using System.Text;
+using Application.Api.GraphQL.Accounts;
 using Application.Api.GraphQL.EfCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,26 +8,37 @@ using Microsoft.EntityFrameworkCore;
 namespace Application.Api.Rest;
 
 [ApiController]
-public class CsvExportController : ControllerBase
+public class ExportController : ControllerBase
 {
     private readonly IDbContextFactory<GraphQlDbContext> _dbContextFactory;
 
-    public CsvExportController(IDbContextFactory<GraphQlDbContext> dbContextFactory)
+    public ExportController(IDbContextFactory<GraphQlDbContext> dbContextFactory)
     {
         _dbContextFactory = dbContextFactory;
     }
 
     [HttpGet]
     [Route("rest/export/statement")]
-    public async Task<ActionResult> GetStatementExport(long accountId)
+    public async Task<ActionResult> GetStatementExport(string accountAddress)
     {
         await using var dbContext = await _dbContextFactory.CreateDbContextAsync();
 
+        if (!ConcordiumSdk.Types.AccountAddress.TryParse(accountAddress, out var parsed))
+            throw new InvalidOperationException("Account does not exist!");
+
+        var baseAddress = new AccountAddress(parsed!.GetBaseAddress().AsString);
+        var account = dbContext.Accounts
+            .AsNoTracking()
+            .SingleOrDefault(account => account.BaseAddress == baseAddress);
+        if (account == null)
+            throw new InvalidOperationException("Account does not exist!");
+
         var query = dbContext.AccountStatementEntries
             .AsNoTracking()
-            .Where(x => x.AccountId == accountId);
+            .Where(x => x.AccountId == account.Id);
 
-        var scalarQuery = query.Select(x => new {
+        var scalarQuery = query.Select(x => new
+        {
             x.Timestamp,
             x.EntryType,
             x.Amount,
@@ -34,10 +46,10 @@ public class CsvExportController : ControllerBase
         var values = await scalarQuery.ToListAsync();
         // TODO Use something like 'CsvHelper' (see 'https://joshclose.github.io/CsvHelper/examples/writing/write-anonymous-type-objects/')?
         var sb = new StringBuilder("Time,Amount (CCD),Label\n");
-        var csv = values.Aggregate(sb, (acc, v) => acc.Append($"{v.Timestamp.ToString("u")},{v.Amount/1e6},{v.EntryType}\n"));
+        var csv = values.Aggregate(sb, (acc, v) => acc.Append($"{v.Timestamp.ToString("u")},{v.Amount / 1e6},{v.EntryType}\n"));
         var result = new FileContentResult(Encoding.ASCII.GetBytes(csv.ToString()), "text/csv")
         {
-            FileDownloadName = $"statement-{accountId}.csv",
+            FileDownloadName = $"statement-{accountAddress}.csv",
         };
         return result;
     }
