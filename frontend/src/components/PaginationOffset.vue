950788d38d3626d1aceca2c9d79dfdc420d09c94
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2023-10-03 16:32:05 +0200

    changed to flex design

--- frontend/src/components/PaginationOffset.vue
+++ frontend/src/components/PaginationOffset.vue
@@ -1,51 +1,51 @@
 <template>
-    <ul class="pagination">
-        <li>
+    <div class="flex-container">
+        <div>
             <button
             type="button"
             :disabled="isInFirstPage"
             @click="onClickFirstPage"
             >
-                First
+                {{ '<<' }}
             </button>
-        </li>
-        <li>
+        </div>
+        <div>
             <button
             type="button"
             :disabled="isInFirstPage"
             @click="onClickPreviousPage"
             >
-                Previous
+                {{ '<' }}
             </button>
-        </li>
-        <!--- Visible Buttons Start -->
-        <li v-for="page in pages" :key="page.name">
+        </div>
+            <div class="flex-container button-container">
             <button
+                v-for="page in pages" :key="page.name"
                 type="button"
                 :disabled="page.isDisabled"
                 :class="{ active: !page.isDisabled }"
                 @click="onClickPage(page.name)"
             > {{ page.name }}</button>
-        </li>
-        <li>
+        </div>
+        <div>
             <button
                 type="button"
                 :disabled="isInLastPage"
                 @click="onClickNextPage"
             >
-                Next
+                >
             </button>
-        </li>
-        <li>
+        </div>
+        <div>
             <button
                 type="button"
                 :disabled="isInLastPage"
                 @click="onClickLastPage"
             >
-                Last
+                >>
             </button>
-        </li>
-    </ul>
+        </div>
+    </div>
 </template>
 <script lang="ts" setup>
 import { NAVIGATION_SIZE, PaginationOffsetInfo } from '../composables/usePaginationOffset'
@@ -67,7 +67,7 @@ const currentPage = computed(() => {
 
 const pages = computed(() => {
     const from = Math.max(1, currentPage.value - NAVIGATION_SIZE);
-    const to = currentPage.value + NAVIGATION_SIZE;
+    const to = Math.min(currentPage.value + NAVIGATION_SIZE, totalPages.value);
     const range = [];
     for (let i = from; i <= to; i++) {
         range.push({name: i, isDisabled: i !== currentPage.value})
@@ -85,12 +85,15 @@ const onClickPreviousPage = () => {
 }
 
 const onClickNextPage = () => {
-    const next = Math.max(0, props.info.skip.value - props.info.take.value);
+    const next = Math.max(0, props.info.skip.value + props.info.take.value);
     props.info.update(next);
 }
 const onClickLastPage = () => {
-    const count = Math.floor(props.totalCount / props.info.take.value)
-    props.info.update(count)
+    const remainder = props.totalCount % props.info.take.value;
+    const toSkip = remainder === 0 ? 
+        props.totalCount - props.info.take.value :
+        props.totalCount - remainder;
+    props.info.update(toSkip)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -98,16 +101,34 @@ const onClickPage = (page: number): void => {};
 
 </script>
 <style>
-.pagination {
-    list-style-type: none;
+div.flex-container {
+    display: flex;
+    border: 2px dashed royalblue;
+    justify-content: center;
+    align-items: center;
 }
 
-.pagination li {
-    display: inline-block;
+div.flex-container > div {
+    padding: 10px 10px;
+}
+
+div.button-container {
+    background-color: var(--color-background-elevated);
+    border-radius: 25px;
+}
+
+div.button-container button {
+    padding: 0 10px;
+}
+
+div > button:hover {
+    background-color: var(--color-background-elevated);
+    opacity: 0.4;
+    border-radius: 25px;
 }
 
 .active {
-    background-color: #4AAE9B;
-    color: #ffffff;
+    background-color: var(--color-background-elevated-hover);
+    border-radius: 25px;
 }
 </style>
