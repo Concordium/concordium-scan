86ae1a897f20bafa22d0235bded7caa535c1ab9b
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2023-10-05 10:13:47 +0200

    Added question mark icon

--- frontend/src/components/atoms/InfoTooltip.vue
+++ frontend/src/components/atoms/InfoTooltip.vue
@@ -6,12 +6,11 @@
 		y="50%"
 		tooltip-position="absolute"
 	>
-		<span style="padding-left: 10px;">
-            ?
-        </span>
+        <QuestionMarkCircleIcon class="inline w-5 h-5" />
 	</Tooltip>
 </template>
 <script lang="ts" setup>
+import { QuestionMarkCircleIcon } from '@heroicons/vue/outline';
 import Tooltip from '~~/src/components/atoms/Tooltip.vue'
 
 type Props = {
