1387945d8424a5dc99a153f8797a7e766740fe97
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2023-10-12 15:57:25 +0200

    change default drop down to 10

--- frontend/src/composables/usePageDropdown.ts
+++ frontend/src/composables/usePageDropdown.ts
@@ -1,13 +1,14 @@
 import { Ref } from "vue"
 
 export const MIN_PAGE_SIZE = 5;
+export const DEFAULT_PAGE_SIZE = 10;
 
 export type PageDropdownInfo = {
     take: Ref<number>
     update: (take: number) => void
 }
 
-export const usePageDropdown = (pageSize: number = MIN_PAGE_SIZE) : PageDropdownInfo => {
+export const usePageDropdown = (pageSize: number = DEFAULT_PAGE_SIZE) : PageDropdownInfo => {
     const _take = ref<number>(pageSize);
 
     const updateTake = (take: number) => {
