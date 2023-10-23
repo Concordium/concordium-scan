c4b11692428013fbbf68b5e404583f8ab725a14a
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2023-10-23 14:04:22 +0200

    Refactored tests

--- backend/Tests/Aggregates/Contract/Types/ModuleSchemaVersionTests.cs
+++ backend/Tests/Aggregates/Contract/Types/ModuleSchemaVersionTests.cs
@@ -6,25 +6,26 @@ namespace Tests.Aggregates.Contract.Types;
 public sealed class ModuleSchemaVersionTests
 {
     [Theory]
-    [InlineData(ModuleSchemaVersion.Undefined, (byte)0)]
     [InlineData(ModuleSchemaVersion.V0, (byte)0)]
     [InlineData(ModuleSchemaVersion.V2, (byte)2)]
-    public void WhenMapModuleSchemaVersionToFFIOption_ThenCorrect(
-        ModuleSchemaVersion version, byte mapped
-        )
+    public void GivenVersion_WhenMapModuleSchemaVersionToFFIOption_ThenOptionContainsVersion(ModuleSchemaVersion version, byte mapped)
     {
         // Act
         var ffiOption = ModuleSchemaVersionExtensions.Into(version);
 
         // Assert
-        if (ModuleSchemaVersion.Undefined == version)
-        {
-            ffiOption.is_some.Should().Be(0);
-        }
-        else
-        {
-            ffiOption.is_some.Should().Be(1);
-            ffiOption.t.Should().Be(mapped);
-        }
+        ffiOption.is_some.Should().Be(1);
+        ffiOption.t.Should().Be(mapped);
     }
+    
+    [Fact]
+    public void GivenUndefinedVersion_WhenMapModuleSchemaVersionToFFIOption_ThenOptionEmpty()
+    {
+        // Act
+        var ffiOption = ModuleSchemaVersionExtensions.Into(ModuleSchemaVersion.Undefined);
+
+        // Assert
+        ffiOption.is_some.Should().Be(0);
+    }
+    
 }
