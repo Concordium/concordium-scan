0bfdafff7ad1d43620906c3ff96b997263be6c28
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2023-11-01 16:40:53 +0100

    Added deserialized to frontend

---
+++ frontend/src/components/Details/Logs.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="w-full">
+		<div>
+			Logs:
+			<InfoTooltip text="Logs produced by contract execution." />
+		</div>
+		<template v-if="events?.nodes?.length">
+			<div v-for="(event, i) in events.nodes" :key="i" class="flex">
+				<code class="truncate w-36">
+					{{ event }}
+				</code>
+				<TextCopy
+					:text="event"
+					label="Click to copy events logs to clipboard"
+				/>
+			</div>
+		</template>
+	</div>
+</template>
+<script lang="ts" setup>
+import { Maybe, StringConnection } from '../../types/generated'
+import TextCopy from '../../components/atoms/TextCopy.vue'
+import InfoTooltip from '../atoms/InfoTooltip.vue'
+
+type Props = {
+	events?: Maybe<StringConnection>
+}
+defineProps<Props>()
+</script>
