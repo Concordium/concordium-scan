4deeafd9706715c77b0c86dcc07e79426350bb85
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2023-11-17 14:21:35 +0100

    Updated migration table

--- backend/DatabaseScripts/SqlScripts/0047_alter_table_graphql_bakers_add_payday_commissions.sql
+++ backend/DatabaseScripts/SqlScripts/0047_alter_table_graphql_bakers_add_payday_commissions.sql
@@ -1,10 +1,19 @@
+/*
+Add columns for current payday commissions.
+
+Values are initialized to those of the latest change to commissions received. They may not be correct since changes
+on the payday of the migration will be wrongly used.
+
+The values will be reset on the next payday block, hence the values will only be wrong until the next payday block
+is received.
+*/
 ALTER TABLE graphql_bakers
 ADD COLUMN active_pool_payday_status_transaction_commission     numeric null,
 ADD COLUMN active_pool_payday_status_finalization_commission    numeric null,
 ADD COLUMN active_pool_payday_status_baking_commission          numeric null;
-    
+
 UPDATE graphql_bakers
 SET 
-    active_pool_payday_status_transaction_commission = (SELECT active_pool_transaction_commission from graphql_bakers),
-    active_pool_payday_status_finalization_commission = (SELECT active_pool_finalization_commission from graphql_bakers),
-    active_pool_payday_status_baking_commission = (SELECT active_pool_baking_commission from graphql_bakers);
+    active_pool_payday_status_transaction_commission = active_pool_transaction_commission,
+    active_pool_payday_status_finalization_commission = active_pool_finalization_commission,
+    active_pool_payday_status_baking_commission = active_pool_baking_commission;
