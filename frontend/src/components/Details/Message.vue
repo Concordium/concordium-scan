f94bff0858fb382ed7d5a75e22396da5682bda41
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2023-11-19 11:28:34 +0100

    Initial modal

--- frontend/src/components/Details/Message.vue
+++ frontend/src/components/Details/Message.vue
@@ -8,6 +8,14 @@
 			<code class="deserialized-message">
 				{{ message }}
 			</code>
+			<button
+				class="relative transition-colors text-theme-faded hover:text-theme-interactiveHover inline"
+				style="width: 24px"
+				@click="toggleModalVisible"
+			>
+				<ArrowsPointingOut class="inline align-text-top h-4" />
+			</button>
+			<Modal v-show="isVisible" @close="closeModal" />
 			<TextCopy
 				v-if="message"
 				:text="message"
@@ -19,9 +27,19 @@
 <script lang="ts" setup>
 import InfoTooltip from '../atoms/InfoTooltip.vue'
 import TextCopy from '../atoms/TextCopy.vue'
+import Modal from './Modal.vue'
+import ArrowsPointingOut from '~/components/icons/ArrowsPointingOut.vue'
 
 type Props = {
 	message: string
 }
 defineProps<Props>()
+
+const isVisible = ref(false)
+const toggleModalVisible = () => {
+	isVisible.value = !isVisible.value
+}
+const closeModal = () => {
+	isVisible.value = false
+}
 </script>
---
+++ frontend/src/components/Details/Modal.vue
@@ -0,0 +1,101 @@
+<template>
+	<transition name="modal-fade">
+		<div class="modal-backdrop">
+			<div class="modal">
+				<header class="modal-header">
+					<slot name="header"> This is the default title! </slot>
+					<button type="button" class="btn-close" @click="close">x</button>
+				</header>
+				<section class="modal-body">
+					<slot name="body"> This is the default body! </slot>
+				</section>
+				<footer class="modal-footer">
+					<slot name="footer"> This is the default footer! </slot>
+					<button type="button" class="btn-green" @click="close">
+						Close Modal
+					</button>
+				</footer>
+			</div>
+		</div>
+	</transition>
+</template>
+<script setup lang="ts">
+const emit = defineEmits(['close'])
+const close = () => {
+	emit('close')
+}
+</script>
+<style>
+.modal-fade-enter-from,
+.modal-fade-leave-to {
+	opacity: 0;
+}
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+	transition: opacity 0.5s ease;
+}
+.modal-backdrop {
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background-color: rgba(0, 0, 0, 0.3);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.modal {
+	background: #ffffff;
+	box-shadow: 2px 2px 20px 1px;
+	overflow-x: auto;
+	display: flex;
+	flex-direction: column;
+}
+
+.modal-header,
+.modal-footer {
+	padding: 15px;
+	display: flex;
+}
+
+.modal-header {
+	position: relative;
+	border-bottom: 1px solid #eeeeee;
+	color: #4aae9b;
+	justify-content: space-between;
+}
+
+.modal-footer {
+	border-top: 1px solid #eeeeee;
+	flex-direction: column;
+	justify-content: flex-end;
+}
+
+.modal-body {
+	color: #4aae9b;
+	position: relative;
+	padding: 20px 10px;
+}
+
+.btn-close {
+	position: absolute;
+	top: 0;
+	right: 0;
+	border: none;
+	font-size: 20px;
+	padding: 10px;
+	cursor: pointer;
+	font-weight: bold;
+	color: #4aae9b;
+	background: transparent;
+}
+
+.btn-green {
+	color: white;
+	background: #4aae9b;
+	border: 1px solid #4aae9b;
+	border-radius: 2px;
+}
+</style>
