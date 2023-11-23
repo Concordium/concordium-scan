8b4d7f3ef8ae239b3b4df5e861e92d8adbb8adf4
Søren Schwartz <132270889+schwartz-concordium@users.noreply.github.com>
2023-11-23 14:19:35 +0100

    updating icons (#142)

--- frontend/src/components/PassiveDelegation/PassiveDelegationHeader.vue
+++ frontend/src/components/PassiveDelegation/PassiveDelegationHeader.vue
@@ -1,5 +1,6 @@
 ﻿<template>
 	<DrawerTitle class="flex flex-row flex-wrap">
+		<DelegationIcon class="w-12 h-12 mr-4 hidden md:block" />
 		<div class="flex flex-wrap flex-grow w-1/2">
 			<div class="w-full flex items-center justify-items-stretch">
 				<h1 class="inline-block text-2xl">Passive Delegation</h1>
@@ -9,5 +10,6 @@
 </template>
 
 <script lang="ts" setup>
+import DelegationIcon from '../icons/DelegationIcon.vue'
 import DrawerTitle from '~/components/Drawer/DrawerTitle.vue'
 </script>
