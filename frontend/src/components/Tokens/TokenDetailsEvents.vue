1cbf914e565bba1062837d04166cb8c1291c9b88
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2024-01-15 11:45:48 +0100

    added parsed event

--- frontend/src/components/Tokens/TokenDetailsEvents.vue
+++ frontend/src/components/Tokens/TokenDetailsEvents.vue
@@ -52,6 +52,35 @@
 							:fraction-digits="Number(decimals || 0)"
 						/>
 					</div>
+					<div v-if="tokenEvent.event.parsed" class="w-full">
+						<div>
+							Event:
+							<InfoTooltip text="Full event" />
+						</div>
+						<div class="flex">
+							<code class="truncate w-96">
+								{{ tokenEvent.event.parsed }}
+							</code>
+							<Modal header-title="Event">
+								<template #body>
+									<code style="text-align: left">
+										<pre>{{
+											JSON.stringify(
+												JSON.parse(tokenEvent.event.parsed),
+												null,
+												2
+											)
+										}}</pre>
+									</code>
+								</template>
+							</Modal>
+							<TextCopy
+								v-if="tokenEvent.event.parsed"
+								:text="tokenEvent.event.parsed"
+								label="Click to copy event to clipboard"
+							/>
+						</div>
+					</div>
 				</DetailsView>
 			</TableTd>
 		</TableRow>
@@ -63,6 +92,9 @@ import DetailsView from '../Details/DetailsView.vue'
 import ContractLink from '../molecules/ContractLink.vue'
 import AccountLink from '../molecules/AccountLink.vue'
 import TokenAmount from '../atoms/TokenAmount.vue'
+import InfoTooltip from '../atoms/InfoTooltip.vue'
+import Modal from '../Details/Modal.vue'
+import TextCopy from '../atoms/TextCopy.vue'
 import CisTokenMetadataEvent from './Events/CisTokenMetadataEvent.vue'
 import { TokenEvent } from '~~/src/types/generated'
 
