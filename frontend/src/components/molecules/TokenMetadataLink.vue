6eedfd4d0d10fc28f0f44455d5f7ba31cfc7674a
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2024-01-16 16:41:25 +0100

    Change token url not to be links

--- frontend/src/components/molecules/TokenMetadataLink.vue
+++ frontend/src/components/molecules/TokenMetadataLink.vue
@@ -1,20 +1,21 @@
 <template>
 	<div class="inline-block whitespace-nowrap">
-		<TransactionIcon class="h-4 w-4 align-text-top" />
 		<span v-if="props.url">
-			<LinkButton class="px-2" @click="handleOnClick">
-				<Tooltip
-					:text="props.url"
-					text-class="text-theme-body"
-					class="token-metadata-tool-tip"
-				>
+			<Tooltip
+				text="This is an external URL, and its source may be potentially malicious. Only redirect to URLs from sources that are known to be trusted"
+				text-class="text-theme-body"
+			>
+				<span class="icon-warning">
+					<WarningIcon class="h-4 align-middle mr-2" />
+				</span>
+				<span class="token-metadata-tool-tip text-theme-faded">
 					{{ props.url }}
-				</Tooltip>
-			</LinkButton>
+				</span>
+			</Tooltip>
 			<TextCopy
 				:text="props.url"
 				label="Click to copy"
-				class="h-5 inline align-baseline"
+				class="h-5 inline align-baseline ml-2"
 				tooltip-class="font-sans"
 			/>
 		</span>
@@ -22,27 +23,25 @@
 	</div>
 </template>
 <script setup lang="ts">
-import LinkButton from '../atoms/LinkButton.vue'
-import TransactionIcon from '../icons/TransactionIcon.vue'
 import TextCopy from '../atoms/TextCopy.vue'
 import Tooltip from '../atoms/Tooltip.vue'
+import WarningIcon from '../icons/WarningIcon.vue'
 
 type Props = {
 	url?: string
 }
 const props = defineProps<Props>()
-
-const handleOnClick = () => {
-	props.url && window.open(props.url, '_blank')
-}
 </script>
 <style scoped>
+.icon-warning {
+	color: #ffc600;
+}
 .token-metadata-tool-tip {
 	text-align: left;
 	text-overflow: ellipsis;
 	overflow: hidden;
 	white-space: nowrap;
-	display: block;
+	display: inline;
 	@media screen and (min-width: 640px) and (max-width: 900px) {
 		max-width: 350px;
 	}
