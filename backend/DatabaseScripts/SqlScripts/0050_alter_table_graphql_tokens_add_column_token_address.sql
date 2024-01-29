fb9dcbb08beee82d90f61407a429e0a7bdb4c5bf
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2024-01-29 11:06:26 +0100

    Added index

--- backend/DatabaseScripts/SqlScripts/0050_alter_table_graphql_tokens_add_column_token_address.sql
+++ backend/DatabaseScripts/SqlScripts/0050_alter_table_graphql_tokens_add_column_token_address.sql
@@ -1,5 +1,10 @@
 /*
-Add column for token address which consist of contract index, contract subindex and token id. 
+Add column for token address which consist of contract index, contract subindex and token id.
+
+Adds index on the column since it would be needed for searching.
  */
 ALTER TABLE graphql_tokens
 ADD COLUMN token_address text null;
+
+CREATE INDEX graphql_tokens_token_address_index
+    ON graphql_tokens (token_address);
