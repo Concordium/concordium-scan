aa7749bc103a7d511137c00d106cc4df405bd912
Søren Schwartz <132270889+schwartz-concordium@users.noreply.github.com>
2024-02-06 15:37:02 +0100

    Update backend/Application/Database/MigrationJobs/_03_UpdateFinalizationTimes.cs
    Co-authored-by: Thomas Dinsdale-Young <td202@users.noreply.github.com>

--- backend/Application/Database/MigrationJobs/_03_UpdateFinalizationTimes.cs
+++ backend/Application/Database/MigrationJobs/_03_UpdateFinalizationTimes.cs
@@ -19,7 +19,7 @@ namespace Application.Database.MigrationJobs;
 /// Changed calculations of finalization time to use last finalized block hash from block info instead of
 /// using finalization summaries.
 /// 
-/// Changes are needed since finalization summaries isn't present in protocol 6.
+/// Changes are needed since finalization summaries aren't present in protocol 6.
 /// 
 /// Using latest finalization block hash in block info is a robust calculation in both old (before protocol 6)
 /// and new consensus protocol.
