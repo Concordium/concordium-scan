a4965e1d168519bf57ccbf9f7097e906eb0548aa
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2024-02-19 14:52:36 +0100

    Update _06_AddTokenAddress.cs

--- backend/Application/Aggregates/Contract/Jobs/_06_AddTokenAddress.cs
+++ backend/Application/Aggregates/Contract/Jobs/_06_AddTokenAddress.cs
@@ -53,34 +53,19 @@ public sealed class _06_AddTokenAddress : IStatelessJob
             {
                 await using var context = await _contextFactory.CreateDbContextAsync(token);
                 var connection = context.Database.GetDbConnection();
-                var parameter = new { Index = (long)identifier};
-                var tokens = await connection.QueryAsync<Token>(Tokens, parameter);
+                var tokens = await context.Tokens
+                    .AsNoTracking()
+                    .Where(t => (int)t.ContractIndex == identifier && t.TokenAddress == null)
+                    .ToListAsync(token);
                 
-                var stringBuilder = new StringBuilder();
                 foreach (var cisToken in tokens.Where(cisToken => cisToken.TokenAddress == null))
                 {
                     var tokenAddress = Token.EncodeTokenAddress(cisToken.ContractIndex, cisToken.ContractSubIndex, cisToken.TokenId);
-                    stringBuilder.Append(
-                        $"update graphql_tokens set token_address = '{tokenAddress}' where contract_index = {cisToken.ContractIndex} and contract_sub_index = {cisToken.ContractSubIndex} and token_id = '{cisToken.TokenId}'; ");
+                    await connection.ExecuteAsync($"update graphql_tokens set token_address = '{tokenAddress}' where contract_index = {cisToken.ContractIndex} and contract_sub_index = {cisToken.ContractSubIndex} and token_id = '{cisToken.TokenId}';");
                 }
-                if (stringBuilder.Length == 0)
-                {
-                    return;
-                }
-                var query = stringBuilder.ToString();
-                await context.Database.GetDbConnection().ExecuteAsync(query);
             });
         _logger.Debug($"Completed successfully processing {identifier}");
     }
     
     public bool ShouldNodeImportAwait() => true;
-
-    private const string Tokens = @"
-SELECT
-    g0.contract_index as ContractIndex,
-    g0.contract_sub_index as ContractSubIndex,
-    g0.token_id as TokenId
-FROM graphql_tokens AS g0
-WHERE g0.contract_index = @Index
-";    
 }
