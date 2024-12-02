03f74cf193b03a66309a3974fbc6657a0cf0af49
Doris Benda <DOBEN@users.noreply.github.com>
2024-12-02 20:30:41 +0700

    Update total_supply if CIS2::Mint event occurs

--- backend-rust/src/indexer.rs
+++ backend-rust/src/indexer.rs
@@ -9,6 +9,7 @@ use anyhow::Context;
 use chrono::{DateTime, Utc};
 use concordium_rust_sdk::{
     base::{contracts_common::to_bytes, smart_contracts::WasmVersion},
+    cis2::{self},
     common::types::{Amount, Timestamp},
     indexer::{async_trait, Indexer, ProcessEvent, TraverseConfig, TraverseError},
     smart_contracts::engine::utils::{get_embedded_schema_v0, get_embedded_schema_v1},
@@ -24,6 +25,7 @@ use concordium_rust_sdk::{
     },
 };
 use futures::{StreamExt, TryStreamExt};
+use num_bigint::BigUint;
 use prometheus_client::{
     metrics::{
         counter::Counter,
@@ -91,9 +93,9 @@ impl IndexerService {
         config: IndexerServiceConfig,
     ) -> anyhow::Result<Self> {
         let last_height_stored = sqlx::query!(
-            r#"
+            "
 SELECT height FROM blocks ORDER BY height DESC LIMIT 1
-"#
+"
         )
         .fetch_optional(&pool)
         .await?
@@ -106,7 +108,7 @@ SELECT height FROM blocks ORDER BY height DESC LIMIT 1
             1
         };
         let genesis_block_hash: sdk_types::hashes::BlockHash =
-            sqlx::query!(r#"SELECT hash FROM blocks WHERE height=0"#)
+            sqlx::query!("SELECT hash FROM blocks WHERE height=0")
                 .fetch_one(&pool)
                 .await?
                 .hash
@@ -432,28 +434,28 @@ impl BlockProcessor {
         registry: &mut Registry,
     ) -> anyhow::Result<Self> {
         let last_finalized_block = sqlx::query!(
-            r#"
+            "
 SELECT
   hash
 FROM blocks
 WHERE finalization_time IS NOT NULL
 ORDER BY height DESC
 LIMIT 1
-"#
+"
         )
         .fetch_one(&pool)
         .await
         .context("Failed to query data for save context")?;
 
         let last_block = sqlx::query!(
-            r#"
+            "
 SELECT
   slot_time,
   cumulative_num_txs
 FROM blocks
 ORDER BY height DESC
 LIMIT 1
-"#
+"
         )
         .fetch_one(&pool)
         .await
@@ -522,7 +524,7 @@ impl ProcessEvent for BlockProcessor {
         PreparedBlock::batch_save(batch, &mut new_context, &mut tx).await?;
         for block in batch {
             for item in block.prepared_block_items.iter() {
-                item.save(&mut tx).await?;
+                item.save(&mut tx, &self.pool).await?;
             }
             out.push_str(format!("\n- {}:{}", block.height, block.hash).as_str())
         }
@@ -688,8 +690,9 @@ async fn save_genesis_data(endpoint: v2::Endpoint, pool: &PgPool) -> anyhow::Res
                 )))
             });
             sqlx::query!(
-                r#"INSERT INTO bakers (id, staked, restake_earnings, open_status, metadata_url, transaction_commission, baking_commission, finalization_commission)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)"#,
+                "INSERT INTO bakers (id, staked, restake_earnings, open_status, metadata_url, \
+                 transaction_commission, baking_commission, finalization_commission)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
                 index,
                 stake,
                 restake_earnings,
@@ -808,7 +811,7 @@ impl PreparedBlock {
         }
 
         sqlx::query!(
-            r#"INSERT INTO blocks
+            "INSERT INTO blocks
   (height, hash, slot_time, block_time, baker_id, total_amount, total_staked, cumulative_num_txs)
 SELECT * FROM UNNEST(
   $1::BIGINT[],
@@ -819,7 +822,7 @@ SELECT * FROM UNNEST(
   $6::BIGINT[],
   $7::BIGINT[],
   $8::BIGINT[]
-);"#,
+);",
             &heights,
             &hashes,
             &slot_times,
@@ -979,6 +982,7 @@ impl PreparedBlockItem {
     async fn save(
         &self,
         tx: &mut sqlx::Transaction<'static, sqlx::Postgres>,
+        pool: &PgPool,
     ) -> anyhow::Result<()> {
         let tx_idx = sqlx::query_scalar!(
             "INSERT INTO transactions (
@@ -1049,7 +1053,7 @@ impl PreparedBlockItem {
         .await?;
 
         if let Some(prepared_event) = &self.prepared_event {
-            prepared_event.save(tx, tx_idx).await?;
+            prepared_event.save(tx, tx_idx, pool).await?;
         }
 
         Ok(())
@@ -1255,6 +1259,7 @@ impl PreparedEvent {
         &self,
         tx: &mut sqlx::Transaction<'static, sqlx::Postgres>,
         tx_idx: i64,
+        pool: &PgPool,
     ) -> anyhow::Result<()> {
         match self {
             PreparedEvent::AccountCreation(event) => event.save(tx, tx_idx).await,
@@ -1268,7 +1273,7 @@ impl PreparedEvent {
             PreparedEvent::ContractInitialized(event) => event.save(tx, tx_idx).await,
             PreparedEvent::ContractUpdate(events) => {
                 for event in events {
-                    event.save(tx, tx_idx).await?;
+                    event.save(tx, tx_idx, pool).await?;
                 }
                 Ok(())
             }
@@ -1631,7 +1636,7 @@ impl PreparedBakerEvent {
                 restake_earnings,
             } => {
                 sqlx::query!(
-                    r#"INSERT INTO bakers (id, staked, restake_earnings) VALUES ($1, $2, $3)"#,
+                    "INSERT INTO bakers (id, staked, restake_earnings) VALUES ($1, $2, $3)",
                     baker_id,
                     staked,
                     restake_earnings,
@@ -1642,7 +1647,7 @@ impl PreparedBakerEvent {
             PreparedBakerEvent::Remove {
                 baker_id,
             } => {
-                sqlx::query!(r#"DELETE FROM bakers WHERE id=$1"#, baker_id,)
+                sqlx::query!("DELETE FROM bakers WHERE id=$1", baker_id,)
                     .execute(tx.as_mut())
                     .await?;
             }
@@ -1650,7 +1655,7 @@ impl PreparedBakerEvent {
                 baker_id,
                 staked,
             } => {
-                sqlx::query!(r#"UPDATE bakers SET staked = $2 WHERE id=$1"#, baker_id, staked,)
+                sqlx::query!("UPDATE bakers SET staked = $2 WHERE id=$1", baker_id, staked,)
                     .execute(tx.as_mut())
                     .await?;
             }
@@ -1658,7 +1663,7 @@ impl PreparedBakerEvent {
                 baker_id,
                 staked,
             } => {
-                sqlx::query!(r#"UPDATE bakers SET staked = $2 WHERE id=$1"#, baker_id, staked,)
+                sqlx::query!("UPDATE bakers SET staked = $2 WHERE id=$1", baker_id, staked,)
                     .execute(tx.as_mut())
                     .await?;
             }
@@ -1667,7 +1672,7 @@ impl PreparedBakerEvent {
                 restake_earnings,
             } => {
                 sqlx::query!(
-                    r#"UPDATE bakers SET restake_earnings = $2 WHERE id=$1"#,
+                    "UPDATE bakers SET restake_earnings = $2 WHERE id=$1",
                     baker_id,
                     restake_earnings,
                 )
@@ -1679,7 +1684,7 @@ impl PreparedBakerEvent {
                 open_status,
             } => {
                 sqlx::query!(
-                    r#"UPDATE bakers SET open_status = $2 WHERE id=$1"#,
+                    "UPDATE bakers SET open_status = $2 WHERE id=$1",
                     baker_id,
                     *open_status as BakerPoolOpenStatus,
                 )
@@ -1691,7 +1696,7 @@ impl PreparedBakerEvent {
                 metadata_url,
             } => {
                 sqlx::query!(
-                    r#"UPDATE bakers SET metadata_url = $2 WHERE id=$1"#,
+                    "UPDATE bakers SET metadata_url = $2 WHERE id=$1",
                     baker_id,
                     metadata_url
                 )
@@ -1703,7 +1708,7 @@ impl PreparedBakerEvent {
                 commission,
             } => {
                 sqlx::query!(
-                    r#"UPDATE bakers SET transaction_commission = $2 WHERE id=$1"#,
+                    "UPDATE bakers SET transaction_commission = $2 WHERE id=$1",
                     baker_id,
                     commission
                 )
@@ -1715,7 +1720,7 @@ impl PreparedBakerEvent {
                 commission,
             } => {
                 sqlx::query!(
-                    r#"UPDATE bakers SET baking_commission = $2 WHERE id=$1"#,
+                    "UPDATE bakers SET baking_commission = $2 WHERE id=$1",
                     baker_id,
                     commission
                 )
@@ -1727,7 +1732,7 @@ impl PreparedBakerEvent {
                 commission,
             } => {
                 sqlx::query!(
-                    r#"UPDATE bakers SET finalization_commission = $2 WHERE id=$1"#,
+                    "UPDATE bakers SET finalization_commission = $2 WHERE id=$1",
                     baker_id,
                     commission
                 )
@@ -1865,10 +1870,11 @@ impl PreparedContractInitialized {
 }
 
 struct PreparedContractUpdate {
-    trace_element_index: i64,
-    height:              i64,
-    contract_index:      i64,
-    contract_sub_index:  i64,
+    trace_element_index:   i64,
+    height:                i64,
+    contract_index:        i64,
+    contract_sub_index:    i64,
+    potential_cis2_events: Vec<cis2::Event>,
 }
 
 impl PreparedContractUpdate {
@@ -1885,11 +1891,36 @@ impl PreparedContractUpdate {
         let index = i64::try_from(contract_address.index)?;
         let sub_index = i64::try_from(contract_address.subindex)?;
 
+        let potential_cis2_events = match event {
+            ContractTraceElement::Updated {
+                data,
+            } => data.events.iter().filter_map(|log| log.try_into().ok()).collect::<Vec<_>>(),
+            ContractTraceElement::Transferred {
+                from,
+                amount,
+                to,
+            } => vec![],
+            ContractTraceElement::Interrupted {
+                address,
+                events,
+            } => events.iter().filter_map(|log| log.try_into().ok()).collect::<Vec<_>>(),
+            ContractTraceElement::Resumed {
+                address,
+                success,
+            } => vec![],
+            ContractTraceElement::Upgraded {
+                address,
+                from,
+                to,
+            } => vec![],
+        };
+
         Ok(Self {
             trace_element_index,
             height,
             contract_index: index,
             contract_sub_index: sub_index,
+            potential_cis2_events,
         })
     }
 
@@ -1897,9 +1928,10 @@ impl PreparedContractUpdate {
         &self,
         tx: &mut sqlx::Transaction<'static, sqlx::Postgres>,
         transaction_index: i64,
+        pool: &PgPool,
     ) -> anyhow::Result<()> {
         sqlx::query!(
-            r#"INSERT INTO contract_events (
+            "INSERT INTO contract_events (
                 transaction_index,
                 trace_element_index,
                 block_height,
@@ -1908,8 +1940,9 @@ impl PreparedContractUpdate {
                 event_index_per_contract
             )
             VALUES (
-                $1, $2, $3, $4, $5, (SELECT COALESCE(MAX(event_index_per_contract) + 1, 0) FROM contract_events WHERE contract_index = $4 AND contract_sub_index = $5)
-            )"#,
+                $1, $2, $3, $4, $5, (SELECT COALESCE(MAX(event_index_per_contract) + 1, 0) FROM \
+             contract_events WHERE contract_index = $4 AND contract_sub_index = $5)
+            )",
             transaction_index,
             self.trace_element_index,
             self.height,
@@ -1918,6 +1951,72 @@ impl PreparedContractUpdate {
         )
         .execute(tx.as_mut())
         .await?;
+
+        // TODO: it would be nice to check if the token supports CIS2 (CIS-0 standard)
+
+        for log in self.potential_cis2_events.iter() {
+            // The `total_supply` value of a token is inserted/updated here.
+            // Only `Mint` and `Burn` events affect the `total_supply` of a
+            // token.
+            if let cis2::Event::Mint {
+                token_id,
+                amount,
+                owner,
+            } = log
+            {
+                // Fetch the current `total_supply` for the given `token_name`.
+                let row = sqlx::query!(
+                    "
+                    SELECT total_supply FROM tokens WHERE token_name = $1",
+                    // TODO: update token name
+                    "token_name".to_string(),
+                )
+                .fetch_optional(pool)
+                .await?;
+
+                // If current_supply exists, decode it and add the new amount.
+                let new_total_supply = if let Some(row) = row {
+                    let current_supply_bytes = row.total_supply;
+
+                    let decoded_amount: BigUint = BigUint::from_bytes_le(&current_supply_bytes);
+
+                    let updated_amount = decoded_amount + amount.0.clone();
+
+                    updated_amount.to_bytes_le()
+                } else {
+                    amount.0.to_bytes_le()
+                };
+
+                // If `token_name` does not exist, insert it with the `amount` as the
+                // `total_supply` value. If `token_name` exists,  update the
+                // `total_supply` value by adding the `amount` to the existing value.
+                sqlx::query!(
+                    "
+                    INSERT INTO tokens (token_name, contract_index, contract_sub_index, \
+                        total_supply)
+                    VALUES ($1, $2, $3, $4)
+                    ON CONFLICT (token_name)
+                    DO UPDATE SET total_supply = EXCLUDED.total_supply",
+                    // TODO: update token name
+                    "token_name".to_string(),
+                    self.contract_index,
+                    self.contract_sub_index,
+                    new_total_supply
+                )
+                .execute(tx.as_mut())
+                .await?;
+            }
+
+            // The `total_supply` value of a token is inserted/updated here.
+            // Only `Mint` and `Burn` events affect to the `total_supply` of a
+            // token.
+            if let cis2::Event::Burn {
+                token_id,
+                amount,
+                owner,
+            } = log
+            {}
+        }
 
         Ok(())
     }
