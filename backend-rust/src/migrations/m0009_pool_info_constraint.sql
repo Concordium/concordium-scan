ff6e1b01977ff9b46d0829a23f86fc92f6d227a1
Lasse Alm <lassealm2@gmail.com>
2025-02-24 11:05:48 +0100

    updated comments

--- backend-rust/src/migrations/m0009_pool_info_constraint.sql
+++ backend-rust/src/migrations/m0009_pool_info_constraint.sql
@@ -1,3 +1,5 @@
+-- Migration loosening the constraint enforcing `pool_total_staked > 0` to `pool_total_staked >= 0`,
+-- since this is the case for the genesis validator with id 8 on Concordium Mainnet.
 ALTER TABLE bakers
     DROP CONSTRAINT check_pool_total_staked_positive,
     ADD CONSTRAINT check_pool_total_staked_positive CHECK (pool_total_staked >= 0);
