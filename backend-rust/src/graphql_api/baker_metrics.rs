0ec0a788176e7fbc36583f6569b22984eac905ca
Lasse Alm <lassealm2@gmail.com>
2025-03-06 10:21:56 +0100

    start implementing

--- backend-rust/src/graphql_api/baker_metrics.rs
+++ backend-rust/src/graphql_api/baker_metrics.rs
@@ -1,8 +1,9 @@
+use std::sync::Arc;
 use std::time::Duration;
 use async_graphql::{Context, Object, SimpleObject};
-use crate::graphql_api::{ApiResult, MetricsPeriod};
+use crate::graphql_api::{get_pool, ApiError, ApiResult, MetricsPeriod};
 use chrono::{TimeDelta};
-
+use sqlx::postgres::types::PgInterval;
 use crate::scalar_types::{DateTime, TimeSpan};
 
 #[derive(Default)]
@@ -12,15 +13,27 @@ pub(crate) struct QueryBakerMetrics;
 impl QueryBakerMetrics {
 
     async fn baker_metrics<'a>(&self, ctx: &Context<'a>, period: MetricsPeriod) ->  ApiResult<BakerMetrics> {
-        let std_duration = Duration::from_secs(3600);
-        let time_delta = TimeDelta::from_std(std_duration)
-            .expect("Conversion from std::time::Duration to TimeDelta failed");
+        let pool = get_pool(ctx)?;
+
+        let last_cumulative_accounts_created =
+            sqlx::query_scalar!("SELECT COALESCE(MAX(index), 0) FROM accounts")
+                .fetch_one(pool)
+                .await?
+                .expect("coalesced");
+
+        let period_interval: PgInterval = period
+            .as_duration()
+            .try_into()
+            .map_err(|e| ApiError::DurationOutOfRange(Arc::new(e)))?;
+
+        let bucket_width = period.bucket_width();
+
         Ok(BakerMetrics {
             bakers_added: 0,
             bakers_removed: 0,
             last_baker_count: 0,
             buckets: BakerMetricsBuckets {
-                bucket_width: TimeSpan::from(time_delta),
+                bucket_width: TimeSpan(bucket_width),
                 y_bakers_added: vec![],
                 y_last_baker_count: 0,
                 x_time: vec![],
