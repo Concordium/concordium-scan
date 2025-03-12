8d04c9dfeb25c1feec11beca8195026a71c5dccc
Emil Holm Gjørup <eg@concordium.com>
2025-03-12 13:49:57 +0100

    Add migration for recomputing pool_total_staked and pool_delegator_count

--- backend-rust/src/migrations/m0013-fix-removed-delegators-restake.sql
+++ backend-rust/src/migrations/m0013-fix-removed-delegators-restake.sql
@@ -67,3 +67,27 @@ WHERE
     AND accounts.index = latest_set_delegation.sender_index
     -- Only update accounts where the latest target baker is not currently baking.
     AND NOT EXISTS(SELECT TRUE FROM bakers WHERE id = latest_set_delegation.target);
+
+
+-- Migration recomputing the pool_delegator_count and total_delegated_stake
+
+-- First reset every baker the state of having no delegators.
+UPDATE bakers
+    SET pool_total_staked = bakers.staked,
+        pool_delegator_count = 0;
+
+-- Then update bakers with delegators.
+WITH pool_delegators AS (
+     SELECT
+         delegated_target_baker_id,
+         SUM(delegated_stake) as total_delegated_stake,
+         COUNT(*) as delegator_count
+     FROM accounts
+     WHERE delegated_target_baker_id IS NOT NULL
+     GROUP BY delegated_target_baker_id
+)
+UPDATE bakers
+    SET pool_total_staked = bakers.staked + pool_delegators.total_delegated_stake,
+        pool_delegator_count = pool_delegators.delegator_count
+    FROM pool_delegators
+    WHERE bakers.id = pool_delegators.delegated_target_baker_id;
