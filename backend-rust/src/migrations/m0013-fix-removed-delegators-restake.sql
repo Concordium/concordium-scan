5d7827ba85d2b69b28ce4dadbd4dd0ba3971a701
Emil Holm Gjørup <eg@concordium.com>
2025-03-12 13:18:52 +0100

    Add migration fixing data for the missed set delegation target events

--- backend-rust/src/migrations/m0013-fix-removed-delegators-restake.sql
+++ backend-rust/src/migrations/m0013-fix-removed-delegators-restake.sql
@@ -26,6 +26,44 @@ UPDATE accounts
     SET delegated_restake_earnings = NULL
 FROM delegators
 WHERE
-    delegated_restake_earnings <> NULL
+    delegated_restake_earnings IS NOT NULL
     AND accounts.index = delegators.index
     AND delegators.is_delegating = 0;
+
+
+-- Migration which fixes data due to bug in indexer where delegators which set their target to a pools pending for removal did not get updated (only relevant for blocks prior to Protocol Version 7).
+
+-- The approach is to first find the latest set delegation target event for each account and ensure
+-- the target is still a baker, if not we set the target to the passive pool.
+WITH latest_set_delegation AS (
+    SELECT
+        transactions.sender_index,
+        -- Parse out the target baker of from the events of the latest transaction.
+        JSONB_PATH_QUERY(
+            events,
+            '$[*].DelegationSetDelegationTarget.delegation_target.BakerDelegationTarget.baker_id'
+        )::BIGINT as target
+    FROM transactions
+        INNER JOIN (
+            -- Find the latest transaction index for each account containing a 'DelegationSetDelegationTarget' event.
+            SELECT
+                MAX(index) as tx_index,
+                sender_index
+            FROM transactions
+            WHERE
+                type_account = 'ConfigureDelegation'
+                AND events @> '[{"DelegationSetDelegationTarget": {}}]'::JSONB
+            GROUP BY sender_index
+        ) latest ON latest.sender_index = transactions.sender_index
+            AND latest.tx_index = transactions.index
+)
+UPDATE accounts
+    SET delegated_restake_earnings = NULL
+FROM latest_set_delegation
+WHERE
+    -- Only update accounts which are delegating
+    accounts.delegated_restake_earnings IS NOT NULL
+    -- Only update accounts which have a send a 'set delegation target' transaction.
+    AND accounts.index = latest_set_delegation.sender_index
+    -- Only update accounts where the latest target baker is not currently baking.
+    AND NOT EXISTS(SELECT TRUE FROM bakers WHERE id = latest_set_delegation.target);
