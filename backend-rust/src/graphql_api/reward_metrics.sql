6d55b8264570a3d264c1225ae259e26b2672241d
Lasse Alm <lassealm2@gmail.com>
2025-03-14 09:52:20 +0100

    tmp commit

--- backend-rust/src/graphql_api/reward_metrics.sql
+++ backend-rust/src/graphql_api/reward_metrics.sql
@@ -1,6 +1,6 @@
 SELECT
     bucket_time.bucket_start as "bucket_time!",
-    COALESCE(SUM(period.account_period_max), 0) - COALESCE(SUM(period.account_period_min), 0) as "bucket_rewards_total!"
+    COALESCE(after_bucket.accumulated_amount, 0) - COALESCE(before_bucket.accumulated_amount, 0) as "bucket_rewards!"
 FROM
     date_bin_series(
         $3::interval,
@@ -9,10 +9,25 @@ FROM
     ) AS bucket_time
 LEFT JOIN LATERAL (
     SELECT
-        MAX(accumulated_amount) as account_period_max,
-        MIN(accumulated_amount) as account_period_min
+        (CASE
+            WHEN $1 IS NULL THEN total_accumulated_amount
+            ELSE account_accumulated_amount
+        END) AS accumulated_amount
     FROM metrics_rewards
     LEFT JOIN blocks ON metrics_rewards.block_height = blocks.height
     GROUP BY account_id
-    WHERE slot_time < bucket_time.bucket_end AND slot_time >= bucket_time.bucket_start AND ($1 IS NULL OR account_id = $1)
-) period ON true
+    WHERE slot_time < bucket_time.bucket_end AND ($1 IS NULL OR account_id = $1)
+    LIMIT 1
+) before_bucket ON true
+LEFT JOIN LATERAL (
+    SELECT
+        (CASE
+            WHEN $1 IS NULL THEN total_accumulated_amount
+            ELSE account_accumulated_amount
+        END) AS accumulated_amount
+    FROM metrics_rewards
+    LEFT JOIN blocks ON metrics_rewards.block_height = blocks.height
+    GROUP BY account_id
+    WHERE slot_time < bucket_time.bucket_start AND ($1 IS NULL OR account_id = $1)
+    LIMIT 1
+) after_bucket ON true
--- backend-rust/src/migrations/m0014-reward-metrics.sql
+++ backend-rust/src/migrations/m0014-reward-metrics.sql
@@ -1,8 +1,9 @@
 CREATE TABLE metrics_rewards
 (
-    block_height            BIGINT            PRIMARY KEY REFERENCES blocks(height),
-    account_id              BIGINT    NOT NULL,
-    accumulated_amount      BIGINT    NOT NULL
+    block_height                    BIGINT    PRIMARY KEY REFERENCES blocks(height),
+    account_id                      BIGINT    NOT NULL,
+    total_accumulated_amount        BIGINT    NOT NULL,
+    account_accumulated_amount      BIGINT    NOT NULL,
 );
 
 CREATE INDEX ON metrics_rewards (account_id, block_height);
