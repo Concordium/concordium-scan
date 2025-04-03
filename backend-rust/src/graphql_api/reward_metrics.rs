e0f5e5d2f2d3d7ada91224e5149dde398510db42
Emil Holm Gjørup <eg@concordium.com>
2025-04-03 12:08:41 +0200

    Reward metrics only include the end time and not the strt time in each bucket

--- backend-rust/src/graphql_api/reward_metrics.rs
+++ backend-rust/src/graphql_api/reward_metrics.rs
@@ -130,9 +130,15 @@ async fn reward_metrics(
         SELECT
             bucket_time.bucket_start AS "bucket_time!",
             (SELECT COALESCE(SUM(amount), 0)
-                            FROM metrics_rewards
-                            WHERE block_slot_time BETWEEN bucket_time.bucket_start AND bucket_time.bucket_end
-                            AND ($4::BIGINT IS NULL OR account_index = $4::BIGINT))::BIGINT AS "accumulated_amount!"
+                FROM metrics_rewards
+                WHERE
+                    block_slot_time > bucket_time.bucket_start
+                    AND block_slot_time <= bucket_time.bucket_end
+                    AND (
+                        $4::BIGINT IS NULL
+                        OR account_index = $4::BIGINT
+                    )
+            )::BIGINT AS "accumulated_amount!"
         FROM
             date_bin_series(
                 $3::interval,
