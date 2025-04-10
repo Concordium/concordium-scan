d8762f690dfcea915ddba88d1f46f6c1ad8063e6
Lasse Alm <lassealm2@gmail.com>
2025-04-10 11:04:00 +0200

    fix query performance

--- backend-rust/src/migrations/m0028_reindex_reward_metrics.sql
+++ backend-rust/src/migrations/m0028_reindex_reward_metrics.sql
@@ -1,3 +1,5 @@
+-- The primary key has been changed to have block_slot_time as the leading column
+-- because all queries on the metrics_rewards table reduce sums based on block_slot_time.
 ALTER TABLE metrics_rewards
 DROP CONSTRAINT metrics_rewards_pkey;
 
