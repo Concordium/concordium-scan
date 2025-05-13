e0387c5f30d0e6d2bc8494c164507c4a6af4e9e2
Emil Holm Gjørup <eg@concordium.com>
2025-05-13 23:40:39 +0200

    WIP reorganize indexer.rs

---
+++ backend-rust/src/indexer/block.rs
@@ -0,0 +1,268 @@
+use crate::indexer::{
+    block_preprocessor::BlockData, block_processor::BlockProcessingContext, statistics::Statistics,
+};
+use anyhow::Context;
+use block_item::PreparedBlockItem;
+use chrono::{DateTime, Utc};
+use concordium_rust_sdk::v2;
+use protocol_update_migration::ProtocolUpdateMigration;
+use special_transaction_outcomes::{
+    validator_suspension::PreparedUnmarkPrimedForSuspension, PreparedSpecialTransactionOutcomes,
+};
+
+pub mod block_item;
+pub mod protocol_update_migration;
+pub mod special_transaction_outcomes;
+
+/// Preprocessed block which is ready to be saved in the database.
+pub struct PreparedBlock {
+    /// Hash of the block.
+    pub hash:                     String,
+    /// Absolute height of the block.
+    pub height:                   i64,
+    /// Block slot time (UTC).
+    slot_time:                    DateTime<Utc>,
+    /// Id of the validator which constructed the block. Is only None for the
+    /// genesis block.
+    baker_id:                     Option<i64>,
+    /// Total amount of CCD in existence at the time of this block.
+    total_amount:                 i64,
+    /// Total staked CCD at the time of this block.
+    total_staked:                 i64,
+    /// Block hash of the last finalized block.
+    block_last_finalized:         String,
+    /// Preprocessed block items, ready to be saved in the database.
+    prepared_block_items:         Vec<PreparedBlockItem>,
+    /// Preprocessed block special items, ready to be saved in the database.
+    special_transaction_outcomes: PreparedSpecialTransactionOutcomes,
+    /// Unmark the baker and signers of the Quorum Certificate from being primed
+    /// for suspension.
+    baker_unmark_suspended:       PreparedUnmarkPrimedForSuspension,
+    /// Statistics gathered about frequency of events
+    statistics:                   Statistics,
+    /// Optional data migration for when this is the first block after a
+    /// protocol update.
+    protocol_update_migration:    Option<ProtocolUpdateMigration>,
+}
+
+impl PreparedBlock {
+    pub async fn prepare(node_client: &mut v2::Client, data: &BlockData) -> anyhow::Result<Self> {
+        let height = i64::try_from(data.finalized_block_info.height.height)?;
+        let hash = data.finalized_block_info.block_hash.to_string();
+        let block_last_finalized = data.block_info.block_last_finalized.to_string();
+        let slot_time = data.block_info.block_slot_time;
+        let baker_id = if let Some(index) = data.block_info.block_baker {
+            Some(i64::try_from(index.id.index)?)
+        } else {
+            None
+        };
+        let mut statistics = Statistics::new(height, slot_time);
+        let total_amount =
+            i64::try_from(data.tokenomics_info.common_reward_data().total_amount.micro_ccd())?;
+        let total_staked = i64::try_from(data.total_staked_capital.micro_ccd())?;
+        let mut prepared_block_items = Vec::new();
+        for (item_summary, item) in data.events.iter().zip(data.items.iter()) {
+            prepared_block_items.push(
+                PreparedBlockItem::prepare(node_client, data, item_summary, item, &mut statistics)
+                    .await?,
+            )
+        }
+
+        let special_transaction_outcomes = PreparedSpecialTransactionOutcomes::prepare(
+            node_client,
+            &data.block_info,
+            &data.special_events,
+            &mut statistics,
+        )
+        .await?;
+        let baker_unmark_suspended = PreparedUnmarkPrimedForSuspension::prepare(data)?;
+        let protocol_update_migration =
+            ProtocolUpdateMigration::prepare(node_client, data)
+                .await
+                .context("Failed to prepare for data migation caused by protocol update")?;
+        Ok(Self {
+            hash,
+            height,
+            slot_time,
+            baker_id,
+            total_amount,
+            total_staked,
+            block_last_finalized,
+            prepared_block_items,
+            special_transaction_outcomes,
+            baker_unmark_suspended,
+            statistics,
+            protocol_update_migration,
+        })
+    }
+
+    pub async fn batch_save(
+        batch: &[Self],
+        context: &mut BlockProcessingContext,
+        tx: &mut sqlx::PgTransaction<'_>,
+    ) -> anyhow::Result<()> {
+        let mut heights = Vec::with_capacity(batch.len());
+        let mut hashes = Vec::with_capacity(batch.len());
+        let mut slot_times = Vec::with_capacity(batch.len());
+        let mut baker_ids = Vec::with_capacity(batch.len());
+        let mut total_amounts = Vec::with_capacity(batch.len());
+        let mut total_staked = Vec::with_capacity(batch.len());
+        let mut block_times = Vec::with_capacity(batch.len());
+        let mut cumulative_num_txss = Vec::with_capacity(batch.len());
+
+        let mut finalizers = Vec::with_capacity(batch.len());
+        let mut last_finalizeds = Vec::with_capacity(batch.len());
+        let mut finalizers_slot_time = Vec::with_capacity(batch.len());
+
+        for block in batch {
+            heights.push(block.height);
+            hashes.push(block.hash.clone());
+            slot_times.push(block.slot_time);
+            baker_ids.push(block.baker_id);
+            total_amounts.push(block.total_amount);
+            total_staked.push(block.total_staked);
+            block_times.push(
+                block
+                    .slot_time
+                    .signed_duration_since(context.last_block_slot_time)
+                    .num_milliseconds(),
+            );
+            context.last_cumulative_num_txs += block.prepared_block_items.len() as i64;
+            cumulative_num_txss.push(context.last_cumulative_num_txs);
+            context.last_block_slot_time = block.slot_time;
+
+            // Check if this block knows of a new finalized block.
+            // If so, note it down so we can mark the blocks since last time as finalized by
+            // this block.
+            if block.block_last_finalized != context.last_finalized_hash {
+                finalizers.push(block.height);
+                finalizers_slot_time.push(block.slot_time);
+                last_finalizeds.push(block.block_last_finalized.clone());
+
+                context.last_finalized_hash = block.block_last_finalized.clone();
+            }
+        }
+
+        sqlx::query!(
+            "INSERT INTO blocks (
+                height, 
+                hash, 
+                slot_time, 
+                block_time, 
+                baker_id, 
+                total_amount, 
+                total_staked, 
+                cumulative_num_txs
+            )
+            SELECT * FROM UNNEST(
+                $1::BIGINT[],
+                $2::TEXT[],
+                $3::TIMESTAMPTZ[],
+                $4::BIGINT[],
+                $5::BIGINT[],
+                $6::BIGINT[],
+                $7::BIGINT[],
+                $8::BIGINT[]
+            );",
+            &heights,
+            &hashes,
+            &slot_times,
+            &block_times,
+            &baker_ids as &[Option<i64>],
+            &total_amounts,
+            &total_staked,
+            &cumulative_num_txss
+        )
+        .execute(tx.as_mut())
+        .await?;
+
+        // With all blocks in the batch inserted we update blocks which we now can
+        // compute the finalization time for. Using the list of finalizer blocks
+        // (those containing a last finalized block different from its predecessor)
+        // we update the blocks below which does not contain finalization time and
+        // compute it to be the difference between the slot_time of the block and the
+        // finalizer block.
+        sqlx::query!(
+            "UPDATE blocks SET
+                finalization_time = (
+                    EXTRACT(EPOCH FROM finalizer.slot_time - blocks.slot_time)::double precision
+                        * 1000
+                )::bigint,
+                finalized_by = finalizer.height
+            FROM UNNEST(
+                $1::BIGINT[],
+                $2::TEXT[],
+                $3::TIMESTAMPTZ[]
+            ) AS finalizer(height, finalized, slot_time)
+            JOIN blocks last ON finalizer.finalized = last.hash
+            WHERE blocks.finalization_time IS NULL AND blocks.height <= last.height",
+            &finalizers,
+            &last_finalizeds,
+            &finalizers_slot_time
+        )
+        .execute(tx.as_mut())
+        .await?;
+
+        // With the finalization_time update for each finalized block, we also have to
+        // update the cumulative_finalization_time for these blocks.
+        // Returns the cumulative_finalization_time of the latest finalized block.
+        let new_last_cumulative_finalization_time = sqlx::query_scalar!(
+            "WITH cumulated AS (
+                -- Compute the sum of finalization_time for the finalized missing the cumulative.
+                SELECT
+                    height,
+                    -- Note this sum is only of those without a cumulative_finalization_time and
+                    -- not the entire table.
+                    SUM(finalization_time) OVER (
+                        ORDER BY height
+                        RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+                    ) AS time
+                FROM blocks
+                WHERE blocks.cumulative_finalization_time IS NULL
+                    AND blocks.finalization_time IS NOT NULL
+                ORDER BY height
+            ), updated AS (
+                -- Update the cumulative time from the previous known plus the newly computed.
+                UPDATE blocks
+                    SET cumulative_finalization_time = $1 + cumulated.time
+                FROM cumulated
+                WHERE blocks.height = cumulated.height
+                RETURNING cumulated.height, cumulative_finalization_time
+            )
+            -- Return only the latest cumulative_finalization_time.
+            SELECT updated.cumulative_finalization_time
+            FROM updated
+            ORDER BY updated.height DESC
+            LIMIT 1",
+            context.last_cumulative_finalization_time
+        )
+        .fetch_optional(tx.as_mut())
+        .await?
+        .flatten();
+        if let Some(cumulative_finalization_time) = new_last_cumulative_finalization_time {
+            context.last_cumulative_finalization_time = cumulative_finalization_time;
+        }
+        Ok(())
+    }
+
+    pub async fn process_block_content(
+        &self,
+        tx: &mut sqlx::PgTransaction<'_>,
+    ) -> anyhow::Result<()> {
+        if let Some(migration) = self.protocol_update_migration.as_ref() {
+            migration.save(tx).await?;
+        }
+        for item in self.prepared_block_items.iter() {
+            item.save(tx).await.with_context(|| {
+                format!(
+                    "Failed processing block item with hash {} for block height {}",
+                    item.block_item_hash, item.block_height
+                )
+            })?;
+        }
+        self.statistics.save(tx).await?;
+        self.special_transaction_outcomes.save(tx).await?;
+        self.baker_unmark_suspended.save(tx).await?;
+        Ok(())
+    }
+}
---
+++ backend-rust/src/indexer/block_preprocessor.rs
@@ -0,0 +1,322 @@
+use anyhow::Context;
+use concordium_rust_sdk::{
+    base::transactions::{BlockItem, EncodedPayload},
+    common::types::Amount,
+    indexer::TraverseError,
+    types::{
+        self as sdk_types, block_certificates::BlockCertificates, queries::BlockInfo,
+        BlockItemSummary, ProtocolVersion, RewardsOverview, SpecialTransactionOutcome,
+    },
+    v2,
+};
+use futures::TryStreamExt as _;
+use prometheus_client::{
+    metrics::{counter::Counter, family::Family, gauge::Gauge, histogram},
+    registry::Registry,
+};
+use tokio::{time::Instant, try_join};
+use tracing::{debug, error, info};
+
+use super::block::PreparedBlock;
+
+/// State tracked during block preprocessing, this also holds the implementation
+/// of [`Indexer`](concordium_rust_sdk::indexer::Indexer). Since several
+/// preprocessors can run in parallel, this must be `Sync`.
+pub struct BlockPreProcessor {
+    /// Genesis hash, used to ensure the nodes are on the expected network.
+    genesis_hash:                 sdk_types::hashes::BlockHash,
+    /// Metric counting the total number of connections ever established to a
+    /// node.
+    established_node_connections: Family<NodeMetricLabels, Counter>,
+    /// Metric counting the total number of failed attempts to preprocess
+    /// blocks.
+    preprocessing_failures:       Family<NodeMetricLabels, Counter>,
+    /// Metric tracking the number of blocks currently being preprocessed.
+    blocks_being_preprocessed:    Family<NodeMetricLabels, Gauge>,
+    /// Histogram collecting the time it takes for fetching all the block data
+    /// from the node.
+    node_response_time:           Family<NodeMetricLabels, histogram::Histogram>,
+    /// Max number of acceptable successive failures before shutting down the
+    /// service.
+    max_successive_failures:      u64,
+}
+impl BlockPreProcessor {
+    pub fn new(
+        genesis_hash: sdk_types::hashes::BlockHash,
+        max_successive_failures: u64,
+        registry: &mut Registry,
+    ) -> Self {
+        let established_node_connections = Family::default();
+        registry.register(
+            "established_node_connections",
+            "Total number of established Concordium Node connections",
+            established_node_connections.clone(),
+        );
+        let preprocessing_failures = Family::default();
+        registry.register(
+            "preprocessing_failures",
+            "Total number of failed attempts to preprocess blocks",
+            preprocessing_failures.clone(),
+        );
+        let blocks_being_preprocessed = Family::default();
+        registry.register(
+            "blocks_being_preprocessed",
+            "Current number of blocks being preprocessed",
+            blocks_being_preprocessed.clone(),
+        );
+        let node_response_time: Family<NodeMetricLabels, histogram::Histogram> =
+            Family::new_with_constructor(|| {
+                histogram::Histogram::new(histogram::exponential_buckets(0.010, 2.0, 10))
+            });
+        registry.register(
+            "node_response_time_seconds",
+            "Duration of seconds used to fetch all of the block information",
+            node_response_time.clone(),
+        );
+
+        Self {
+            genesis_hash,
+            established_node_connections,
+            preprocessing_failures,
+            blocks_being_preprocessed,
+            node_response_time,
+            max_successive_failures,
+        }
+    }
+}
+
+/// Represents the labels used for metrics related to Concordium Node.
+#[derive(Clone, Debug, Hash, PartialEq, Eq, prometheus_client::encoding::EncodeLabelSet)]
+pub struct NodeMetricLabels {
+    /// URI of the node
+    node: String,
+}
+impl NodeMetricLabels {
+    fn new(endpoint: &v2::Endpoint) -> Self {
+        Self {
+            node: endpoint.uri().to_string(),
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl concordium_rust_sdk::indexer::Indexer for BlockPreProcessor {
+    type Context = NodeMetricLabels;
+    type Data = PreparedBlock;
+
+    /// Called when a new connection is established to the given endpoint.
+    /// The return value from this method is passed to each call of
+    /// on_finalized.
+    async fn on_connect<'a>(
+        &mut self,
+        endpoint: v2::Endpoint,
+        client: &'a mut v2::Client,
+    ) -> v2::QueryResult<Self::Context> {
+        let info = client.get_consensus_info().await?;
+        if info.genesis_block != self.genesis_hash {
+            error!(
+                "Invalid client: {} is on network with genesis hash {} expected {}",
+                endpoint.uri(),
+                info.genesis_block,
+                self.genesis_hash
+            );
+            return Err(v2::QueryError::RPCError(v2::RPCError::CallError(
+                tonic::Status::failed_precondition(format!(
+                    "Invalid client: {} is on network with genesis hash {} expected {}",
+                    endpoint.uri(),
+                    info.genesis_block,
+                    self.genesis_hash
+                )),
+            )));
+        }
+        info!("Connection established to node at uri: {}", endpoint.uri());
+        let label = NodeMetricLabels::new(&endpoint);
+        self.established_node_connections.get_or_create(&label).inc();
+        Ok(label)
+    }
+
+    /// The main method of this trait. It is called for each finalized block
+    /// that the indexer discovers. Note that the indexer might call this
+    /// concurrently for multiple blocks at the same time to speed up indexing.
+    ///
+    /// This method is meant to return errors that are unexpected, and if it
+    /// does return an error the indexer will attempt to reconnect to the
+    /// next endpoint.
+    async fn on_finalized<'a>(
+        &self,
+        mut client: v2::Client,
+        label: &'a Self::Context,
+        fbi: v2::FinalizedBlockInfo,
+    ) -> v2::QueryResult<Self::Data> {
+        self.blocks_being_preprocessed.get_or_create(label).inc();
+        debug!("Preprocessing block {}:{}", fbi.height, fbi.block_hash);
+        // We block together the computation, so we can update the metric in the error
+        // case, before returning early.
+        let result = async move {
+            let mut client1 = client.clone();
+            let mut client2 = client.clone();
+            let mut client3 = client.clone();
+            let mut client4 = client.clone();
+            let mut client5 = client.clone();
+            let mut client6 = client.clone();
+            let get_block_info = async move {
+                let block_info = client1.get_block_info(fbi.height).await?.response;
+                // Fetching the block certificates prior to P6 results in a InvalidArgument gRPC
+                // error, so we produce the empty type of certificates instead.
+                // The information is only used when preparing blocks for P8 and up.
+                let certificates = if block_info.protocol_version < ProtocolVersion::P8 {
+                    BlockCertificates {
+                        quorum_certificate:       None,
+                        timeout_certificate:      None,
+                        epoch_finalization_entry: None,
+                    }
+                } else {
+                    let response = client1.get_block_certificates(fbi.height).await?;
+                    response.response
+                };
+                Ok((block_info, certificates))
+            };
+
+            let get_events = async move {
+                let events = client2
+                    .get_block_transaction_events(fbi.height)
+                    .await?
+                    .response
+                    .try_collect::<Vec<_>>()
+                    .await?;
+                Ok(events)
+            };
+
+            let get_tokenomics_info = async move {
+                let tokenomics_info = client3.get_tokenomics_info(fbi.height).await?.response;
+                let total_staked_capital = match &tokenomics_info {
+                    RewardsOverview::V0 {
+                        ..
+                    } => {
+                        compute_total_stake_capital(
+                            &mut client3,
+                            v2::BlockIdentifier::AbsoluteHeight(fbi.height),
+                        )
+                        .await?
+                    }
+                    RewardsOverview::V1 {
+                        total_staked_capital,
+                        ..
+                    } => *total_staked_capital,
+                };
+                Ok((tokenomics_info, total_staked_capital))
+            };
+
+            let get_items = async move {
+                let items = client4
+                    .get_block_items(fbi.height)
+                    .await?
+                    .response
+                    .try_collect::<Vec<_>>()
+                    .await?;
+                Ok(items)
+            };
+
+            let get_special_items = async move {
+                let items = client5
+                    .get_block_special_events(fbi.height)
+                    .await?
+                    .response
+                    .try_collect::<Vec<_>>()
+                    .await?;
+                Ok(items)
+            };
+            let start_fetching = Instant::now();
+            let (
+                (block_info, certificates),
+                chain_parameters,
+                (tokenomics_info, total_staked_capital),
+                events,
+                items,
+                special_events,
+            ) = try_join!(
+                get_block_info,
+                client6.get_block_chain_parameters(fbi.height),
+                get_tokenomics_info,
+                get_events,
+                get_items,
+                get_special_items
+            )?;
+            let node_response_time = start_fetching.elapsed();
+            self.node_response_time.get_or_create(label).observe(node_response_time.as_secs_f64());
+
+            let data = BlockData {
+                finalized_block_info: fbi,
+                block_info,
+                events,
+                items,
+                chain_parameters: chain_parameters.response,
+                tokenomics_info,
+                total_staked_capital,
+                special_events,
+                certificates,
+            };
+
+            let prepared_block = PreparedBlock::prepare(&mut client, &data)
+                .await
+                .map_err(v2::RPCError::ParseError)?;
+            Ok(prepared_block)
+        }
+        .await;
+        self.blocks_being_preprocessed.get_or_create(label).dec();
+        debug!("Preprocessing block {}:{} completed", fbi.height, fbi.block_hash);
+        result
+    }
+
+    /// Called when either connecting to the node or querying the node fails.
+    /// The number of successive failures without progress is passed to the
+    /// method which should return whether to stop indexing `true` or not
+    /// `false`
+    async fn on_failure(
+        &mut self,
+        endpoint: v2::Endpoint,
+        successive_failures: u64,
+        err: TraverseError,
+    ) -> bool {
+        info!("Failed preprocessing {} times in row: {}", successive_failures, err);
+        self.preprocessing_failures.get_or_create(&NodeMetricLabels::new(&endpoint)).inc();
+        successive_failures > self.max_successive_failures
+    }
+}
+
+/// Compute the total stake capital by summing all the stake of the bakers.
+/// This is only needed for older blocks, which does not provide this
+/// information as part of the tokenomics info query.
+pub async fn compute_total_stake_capital(
+    client: &mut v2::Client,
+    block_height: v2::BlockIdentifier,
+) -> v2::QueryResult<Amount> {
+    let mut total_staked_capital = Amount::zero();
+    let mut bakers = client.get_baker_list(block_height).await?.response;
+    while let Some(baker_id) = bakers.try_next().await? {
+        let account_info = client
+            .get_account_info(&v2::AccountIdentifier::Index(baker_id.id), block_height)
+            .await?
+            .response;
+        total_staked_capital += account_info
+            .account_stake
+            .context("Expected baker to have account stake information")
+            .map_err(v2::RPCError::ParseError)?
+            .staked_amount();
+    }
+    Ok(total_staked_capital)
+}
+
+/// Raw block information fetched from a Concordium Node.
+pub struct BlockData {
+    pub finalized_block_info: v2::FinalizedBlockInfo,
+    pub block_info:           BlockInfo,
+    pub events:               Vec<BlockItemSummary>,
+    pub items:                Vec<BlockItem<EncodedPayload>>,
+    pub chain_parameters:     v2::ChainParameters,
+    pub tokenomics_info:      RewardsOverview,
+    pub total_staked_capital: Amount,
+    pub special_events:       Vec<SpecialTransactionOutcome>,
+    /// Certificates included in the block.
+    pub certificates:         BlockCertificates,
+}
