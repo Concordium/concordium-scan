90dc2fdb0b316bb9fbd8b8c48e9554c56ab77a5d
Emil Holm Gjørup <eg@concordium.com>
2025-05-21 11:40:29 +0200

    Add entry_type range comment to migration

--- backend-rust/src/migrations/m0034_reindex_account_statement_entry_type_rewards_idx.sql
+++ backend-rust/src/migrations/m0034_reindex_account_statement_entry_type_rewards_idx.sql
@@ -8,4 +8,5 @@ CREATE INDEX account_statements_idx
 -- Efficient lookup of rewards from account_statements for a specific account.
 CREATE INDEX account_statements_entry_type_rewards_idx
   ON account_statements (account_index, id, entry_type)
+  -- Range covers entry types related to rewards: 'FinalizationReward', 'FoundationReward', 'BakerReward', 'TransactionFeeReward'
   WHERE entry_type BETWEEN 'FinalizationReward' AND 'TransactionFeeReward';
