0cee8ad07ef3c759824cb6281a7cf256adf0aa88
Pratap2018 <pratapmridha@gmail.com>
2025-09-03 12:06:30 +0530

    fix CRE-531

---
+++ frontend/src/components/TransactionEventList/Events/PltTransferMemo.vue
@@ -0,0 +1,23 @@
+<template>
+	<div
+		v-if="memo?.__typename === 'Memo'"
+		class="bg-gray rounded flex items-center gap-2"
+	>
+		<span>Memo:</span>
+		<span class="break-all whitespace-pre-line text-xs">{{
+			memo?.bytes ? Buffer.from(memo.bytes, 'hex').toString('utf-8') : ''
+		}}</span>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { Buffer } from 'buffer'
+
+import type { Memo } from '../../../types/generated'
+
+type Props = {
+	memo?: Memo
+}
+
+defineProps<Props>()
+</script>
