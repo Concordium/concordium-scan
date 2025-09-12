07046c2e7d0915cb54c416fb555893d9da79cdb4
Pratap2018 <pratapmridha@gmail.com>
2025-09-12 15:43:14 +0530

    fix comments

--- backend/src/migrations/m0043-plt-event-index-for-pause-unpause.sql
+++ backend/src/migrations/m0043-plt-event-index-for-pause-unpause.sql
@@ -1,6 +1,6 @@
 -- This index is designed to optimize queries that retrieve the latest Pause and Unpause status changes
 -- for a specific token. It orders the results by transaction_index in descending order to ensure
--- that the most recent events are prioritized. The index includes additional columns to cover
+-- that the most recent events are prioritized. 
 CREATE INDEX idx_pause_unpause_latest
 ON plt_events (token_index, transaction_index DESC)
 INCLUDE (id, event_type, token_module_type, token_event)
