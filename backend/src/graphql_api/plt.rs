c62b4d8021ce47a1c7136479b56fe885e7f7f9f0
Pratap2018 <pratapmridha@gmail.com>
2025-10-24 00:27:44 +0530

    PR comments

--- backend/src/graphql_api/plt.rs
+++ backend/src/graphql_api/plt.rs
@@ -348,7 +348,6 @@ impl QueryPlt {
                 total_minted,
                 total_burned,
                 normalized_current_supply::DOUBLE PRECISION AS normalized_current_supply,
-                total_holders,
                 decimal
             FROM plt_tokens 
             WHERE $2 < index AND index < $1
@@ -400,7 +399,6 @@ pub struct PltToken {
     total_minted: Option<BigDecimal>,
     total_burned: Option<BigDecimal>,
     normalized_current_supply: Option<f64>,
-    total_holders: Option<i32>,
     decimal: Option<i32>,
 }
 
@@ -419,7 +417,6 @@ impl PltToken {
                     total_minted,
                     total_burned,
                     normalized_current_supply as "normalized_current_supply: f64",
-                    total_holders,
                     decimal
             FROM plt_tokens WHERE token_id = $1"#,
             token_id.to_string()
@@ -517,8 +514,16 @@ impl PltToken {
         Ok(self.index)
     }
 
-    async fn total_unique_holders(&self) -> ApiResult<i64> {
-        Ok(self.total_holders.unwrap_or(0) as i64)
+    async fn total_unique_holders<'a>(&self, ctx: &Context<'a>) -> ApiResult<i64> {
+        let pool = get_pool(ctx)?;
+        let unique_holder = PltAccountAmount::query_by_token_id(pool, self.token_id.clone())
+            .await
+            .map_err(|e| {
+                ApiError::InternalServerError(InternalError::InternalError(e.to_string()))
+            })?
+            .len() as i64;
+
+        Ok(unique_holder)
     }
 
     async fn token_creation_details(&self, ctx: &Context<'_>) -> ApiResult<TokenCreationDetails> {
@@ -784,27 +789,26 @@ impl PltAccountAmount {
         .await?;
         Ok(result)
     }
-    // TODO: Remove the commented function if trigger is finalized and everyone accepts it.
 
-    // pub async fn query_by_token_id(pool: &PgPool, token_id: TokenId) -> ApiResult<Vec<Self>> {
-    //     let token: PltToken = PltToken::query_by_id(pool, token_id)
-    //         .await?
-    //         .ok_or(ApiError::NotFound)?;
-    //     let result = sqlx::query_as!(
-    //         PltAccountAmount,
-    //         r#"SELECT
-    //             account_index,
-    //             token_index,
-    //             amount,
-    //             decimal
-    //         FROM plt_accounts
-    //         WHERE token_index = $1"#,
-    //         token.index
-    //     )
-    //     .fetch_all(pool)
-    //     .await?;
-    //     Ok(result)
-    // }
+    pub async fn query_by_token_id(pool: &PgPool, token_id: TokenId) -> ApiResult<Vec<Self>> {
+        let token: PltToken = PltToken::query_by_id(pool, token_id)
+            .await?
+            .ok_or(ApiError::NotFound)?;
+        let result = sqlx::query_as!(
+            PltAccountAmount,
+            r#"SELECT
+                account_index,
+                token_index,
+                amount,
+                decimal
+            FROM plt_accounts
+            WHERE token_index = $1"#,
+            token.index
+        )
+        .fetch_all(pool)
+        .await?;
+        Ok(result)
+    }
 
     // unique_accounts means all the accounts that currently hold a non zero
     // balance of any plt token Union of all accounts with a non zero balance
